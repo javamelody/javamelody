19d439b7b34c5248f44a096a699f0047b00c943c
evernat <evernat@free.fr>
2009-09-26 09:50:28 +0000

    compléments tests unitaires

--- javamelody-core/src/test/java/net/bull/javamelody/TestMonitoringFilter.java
+++ javamelody-core/src/test/java/net/bull/javamelody/TestMonitoringFilter.java
@@ -28,6 +28,8 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -134,14 +136,144 @@ public class TestMonitoringFilter {
 	 * @throws ServletException e 
 	 * @throws IOException e */
 	@Test
-	public void testdoMonitoring() throws ServletException, IOException {
+	public void testDoMonitoring() throws ServletException, IOException {
+		monitoring(Collections.<String, String> emptyMap());
+	}
+
+	/** Test.
+	 * @throws ServletException e 
+	 * @throws IOException e */
+	@Test
+	public void testDoMonitoringWithPeriod() throws ServletException, IOException {
+		monitoring(Collections.<String, String> singletonMap("period", "jour"));
+	}
+
+	/** Test.
+	 * @throws ServletException e 
+	 * @throws IOException e */
+	@Test
+	public void testDoMonitoringWithResource() throws ServletException, IOException {
+		monitoring(Collections.<String, String> singletonMap("resource", "monitoring.css"));
+	}
+
+	/** Test.
+	 * @throws ServletException e 
+	 * @throws IOException e */
+	@Test
+	public void testDoMonitoringWithGraph() throws ServletException, IOException {
+		final Map<String, String> parameters = new HashMap<String, String>();
+		parameters.put("graph", "usedMemory");
+		parameters.put("width", "800");
+		parameters.put("height", "600");
+		monitoring(parameters);
+	}
+
+	/** Test.
+	 * @throws ServletException e 
+	 * @throws IOException e */
+	@Test
+	public void testDoMonitoringWithParts() throws ServletException, IOException {
+		final Map<String, String> parameters = new HashMap<String, String>();
+
+		parameters.put(MonitoringController.PART_PARAMETER,
+				MonitoringController.CURRENT_REQUESTS_PART);
+		monitoring(parameters);
+
+		setProperty(Parameter.SYSTEM_ACTIONS_ENABLED, "true");
+		parameters.put(MonitoringController.PART_PARAMETER, MonitoringController.PROCESSES_PART);
+		monitoring(parameters);
+		parameters.put(MonitoringController.PART_PARAMETER, MonitoringController.HEAP_HISTO_PART);
+		monitoring(parameters);
+		parameters.put(MonitoringController.PART_PARAMETER, MonitoringController.SESSIONS_PART);
+		monitoring(parameters);
+		parameters.put(MonitoringController.PART_PARAMETER, MonitoringController.SESSIONS_PART);
+		parameters.put(MonitoringController.SESSION_ID_PARAMETER, "expired session");
+		monitoring(parameters);
+		parameters.put(MonitoringController.PART_PARAMETER, MonitoringController.WEB_XML_PART);
+		monitoring(parameters, false);
+		parameters.put(MonitoringController.PART_PARAMETER, MonitoringController.POM_XML_PART);
+		monitoring(parameters, false);
+
+		parameters.put(MonitoringController.PART_PARAMETER, "graph");
+		parameters.put("graph", "usedMemory");
+		monitoring(parameters);
+	}
+
+	/** Test.
+	 * @throws ServletException e 
+	 * @throws IOException e */
+	@Test
+	public void testDoMonitoringWithFormatPdf() throws ServletException, IOException {
+		monitoring(Collections.<String, String> singletonMap("format", "pdf"));
+	}
+
+	/** Test.
+	 * @throws ServletException e 
+	 * @throws IOException e */
+	@Test
+	public void testDoMonitoringWithFormatSerialized() throws ServletException, IOException {
+		setProperty(Parameter.SYSTEM_ACTIONS_ENABLED, "true");
+		final Map<String, String> parameters = new HashMap<String, String>();
+		parameters.put("format", TransportFormat.SERIALIZED.getCode());
+		monitoring(parameters);
+		parameters.put(MonitoringController.PART_PARAMETER, MonitoringController.SESSIONS_PART);
+		monitoring(parameters);
+		parameters.put(MonitoringController.PART_PARAMETER, MonitoringController.PROCESSES_PART);
+		monitoring(parameters);
+		parameters.put(MonitoringController.PART_PARAMETER, MonitoringController.HEAP_HISTO_PART);
+		monitoring(parameters);
+	}
+
+	/** Test.
+	 * @throws ServletException e 
+	 * @throws IOException e */
+	@Test
+	public void testDoMonitoringWithFormatXml() throws ServletException, IOException {
+		setProperty(Parameter.SYSTEM_ACTIONS_ENABLED, "true");
+		final Map<String, String> parameters = new HashMap<String, String>();
+		parameters.put("format", TransportFormat.XML.getCode());
+		monitoring(parameters);
+		parameters.put(MonitoringController.PART_PARAMETER, MonitoringController.SESSIONS_PART);
+		monitoring(parameters);
+		parameters.put(MonitoringController.PART_PARAMETER, MonitoringController.PROCESSES_PART);
+		monitoring(parameters);
+		parameters.put(MonitoringController.PART_PARAMETER, MonitoringController.HEAP_HISTO_PART);
+		monitoring(parameters);
+	}
+
+	/** Test.
+	 * @throws ServletException e 
+	 * @throws IOException e */
+	@Test
+	public void testDoMonitoringWithFormatJson() throws ServletException, IOException {
+		setProperty(Parameter.SYSTEM_ACTIONS_ENABLED, "true");
+		final Map<String, String> parameters = new HashMap<String, String>();
+		parameters.put("format", TransportFormat.JSON.getCode());
+		monitoring(parameters);
+		parameters.put(MonitoringController.PART_PARAMETER, MonitoringController.SESSIONS_PART);
+		monitoring(parameters);
+		parameters.put(MonitoringController.PART_PARAMETER, MonitoringController.PROCESSES_PART);
+		monitoring(parameters);
+		parameters.put(MonitoringController.PART_PARAMETER, MonitoringController.HEAP_HISTO_PART);
+		monitoring(parameters);
+	}
+
+	private void monitoring(Map<String, String> parameters) throws IOException, ServletException {
+		monitoring(parameters, true);
+	}
+
+	private void monitoring(Map<String, String> parameters, boolean checkResultContent)
+			throws IOException, ServletException {
 		setUp();
 
 		final HttpServletRequest request = createNiceMock(HttpServletRequest.class);
 		expect(request.getRequestURI()).andReturn("/test/monitoring").anyTimes();
 		expect(request.getContextPath()).andReturn("/test").anyTimes();
 		expect(request.getHeaders("Accept-Encoding")).andReturn(
-				Collections.enumeration(Arrays.asList("application/gzip")));
+				Collections.enumeration(Arrays.asList("application/gzip"))).anyTimes();
+		for (final Map.Entry<String, String> entry : parameters.entrySet()) {
+			expect(request.getParameter(entry.getKey())).andReturn(entry.getValue()).anyTimes();
+		}
 		final HttpServletResponse response = createNiceMock(HttpServletResponse.class);
 		final ByteArrayOutputStream output = new ByteArrayOutputStream();
 		expect(response.getOutputStream()).andReturn(new FilterServletOutputStream(output))
@@ -161,7 +293,13 @@ public class TestMonitoringFilter {
 		verify(response);
 		verify(chain);
 
-		assertTrue("result", output.size() != 0);
+		if (checkResultContent) {
+			assertTrue("result", output.size() != 0);
+		}
+	}
+
+	private static void setProperty(Parameter parameter, String value) {
+		System.setProperty(Parameters.PARAMETER_SYSTEM_PREFIX + parameter.getCode(), value);
 	}
 
 	// on ne teste pas MonitoringFilter.destroy car si le timer de JRobin est arrêté,
