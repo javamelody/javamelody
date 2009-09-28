11a2400dfafc530f46fe700d680c6fa9d38fcda7
evernat <evernat@free.fr>
2009-09-28 18:10:46 +0000

    compléments tests unitaires

---
+++ javamelody-core/src/test/java/net/bull/javamelody/TestCollectorServlet.java
@@ -0,0 +1,149 @@
+/*
+ * Copyright 2008-2009 by Emeric Vernat, Bull
+ *
+ *     This file is part of Java Melody.
+ *
+ * Java Melody is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Java Melody is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Java Melody.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package net.bull.javamelody;
+
+import static org.easymock.EasyMock.createNiceMock;
+import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.replay;
+import static org.easymock.EasyMock.verify;
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Test unitaire de la classe CollectorServlet.
+ * @author Emeric Vernat
+ */
+public class TestCollectorServlet {
+	private ServletConfig config;
+	private ServletContext context;
+	private CollectorServlet collectorServlet;
+
+	/**
+	 * Initialisation.
+	 */
+	@Before
+	public void setUp() {
+		config = createNiceMock(ServletConfig.class);
+		context = createNiceMock(ServletContext.class);
+		expect(config.getServletContext()).andReturn(context).anyTimes();
+		collectorServlet = new CollectorServlet();
+	}
+
+	/**
+	 * Terminaison.
+	 */
+	@After
+	public void tearDown() {
+		// on désactive le stop sur le timer JRobin car sinon les tests suivants ne fonctionneront
+		// plus si ils utilisent JRobin
+		System.setProperty(Parameters.PARAMETER_SYSTEM_PREFIX + "jrobinStopDisabled", "true");
+		if (collectorServlet != null) {
+			collectorServlet.destroy();
+		}
+	}
+
+	/** Test.
+	 * @throws ServletException e */
+	@Test
+	public void testInit() throws ServletException {
+		replay(config);
+		replay(context);
+		collectorServlet.init(config);
+		verify(config);
+		verify(context);
+
+		setUp();
+		expect(
+				context.getInitParameter(Parameters.PARAMETER_SYSTEM_PREFIX
+						+ Parameter.LOG.getCode())).andReturn("true").anyTimes();
+		expect(
+				context.getInitParameter(Parameters.PARAMETER_SYSTEM_PREFIX
+						+ Parameter.ALLOWED_ADDR_PATTERN.getCode())).andReturn("127\\.0\\.0\\.1")
+				.anyTimes();
+		replay(config);
+		replay(context);
+		collectorServlet.init(config);
+		verify(config);
+		verify(context);
+	}
+
+	/** Test.
+	 * @throws ServletException e 
+	 * @throws IOException e */
+	@Test
+	public void testDoGet() throws ServletException, IOException {
+		final HttpServletRequest request = createNiceMock(HttpServletRequest.class);
+		expect(request.getRequestURI()).andReturn("/test/request").anyTimes();
+		final HttpServletResponse response = createNiceMock(HttpServletResponse.class);
+		final StringWriter stringWriter = new StringWriter();
+		expect(response.getWriter()).andReturn(new PrintWriter(stringWriter)).anyTimes();
+		replay(config);
+		replay(context);
+		replay(request);
+		replay(response);
+		collectorServlet.init(config);
+		// note: sans serveur de http, il n'est pas possible d'avoir une application et un collector
+		collectorServlet.doGet(request, response);
+		verify(config);
+		verify(context);
+		verify(request);
+		verify(response);
+		assertTrue("result", stringWriter.getBuffer().length() != 0);
+	}
+
+	/** Test.
+	 * @throws ServletException e 
+	 * @throws IOException e */
+	@Test
+	public void testDoPost() throws ServletException, IOException {
+		final HttpServletRequest request = createNiceMock(HttpServletRequest.class);
+		expect(request.getRequestURI()).andReturn("/test/request").anyTimes();
+		final HttpServletResponse response = createNiceMock(HttpServletResponse.class);
+		final StringWriter stringWriter = new StringWriter();
+		expect(response.getWriter()).andReturn(new PrintWriter(stringWriter)).anyTimes();
+		expect(request.getParameter("appName")).andReturn("test");
+		expect(request.getParameter("appUrls")).andReturn("http://localhost:8090/test");
+		replay(config);
+		replay(context);
+		replay(request);
+		replay(response);
+		collectorServlet.init(config);
+		// TODO
+		collectorServlet.doPost(request, response);
+		verify(config);
+		verify(context);
+		verify(request);
+		verify(response);
+		assertTrue("result", stringWriter.getBuffer().length() != 0);
+		System.out.println(stringWriter.getBuffer());
+	}
+}
