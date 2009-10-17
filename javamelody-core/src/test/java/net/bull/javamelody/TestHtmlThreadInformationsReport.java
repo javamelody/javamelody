23c1e15926905e13ff3b135b21f17f1700dc1a8b
evernat <evernat@free.fr>
2009-10-17 21:30:54 +0000

    compléments tests unitaires

---
+++ javamelody-core/src/test/java/net/bull/javamelody/TestHtmlThreadInformationsReport.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2008-2009 by Emeric Vernat, Bull
+ *
+ *     This file is part of Java Melody.
+ *
+ * Java Melody is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Java Melody is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Java Melody.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package net.bull.javamelody;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.Test;
+
+/**
+ * Test unitaire de la classe HtmlSessionInformationsReport.
+ * @author Emeric Vernat
+ */
+public class TestHtmlThreadInformationsReport {
+	private static void assertNotEmptyAndClear(StringWriter writer) {
+		assertTrue("rapport vide", writer.getBuffer().length() > 0);
+		writer.getBuffer().setLength(0);
+	}
+
+	/** Test.
+	 * @throws IOException e */
+	@Test
+	public void testSessionsInformations() throws IOException {
+		final StringWriter writer = new StringWriter();
+		new HtmlThreadInformationsReport(Collections.<ThreadInformations> emptyList(), true, writer)
+				.toHtml();
+		assertNotEmptyAndClear(writer);
+		new HtmlThreadInformationsReport(JavaInformations.buildThreadInformationsList(), true,
+				writer).toHtml();
+		assertNotEmptyAndClear(writer);
+
+		final List<ThreadInformations> threads = new ArrayList<ThreadInformations>();
+		final Thread thread = Thread.currentThread();
+		final List<StackTraceElement> stackTrace = Arrays.asList(thread.getStackTrace());
+		threads.add(new ThreadInformations(thread, stackTrace, 10, 10, true));
+		threads.add(new ThreadInformations(thread, stackTrace, 10, 10, false));
+		new HtmlThreadInformationsReport(threads, true, writer).writeDeadlocks();
+		assertNotEmptyAndClear(writer);
+	}
+}
