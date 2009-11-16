db01df04d232eca94654e24762e9bbaad77bc886
evernat@free.fr <evernat@free.fr@80d463d6-a79d-11de-ba6e-69e41c8db662>
2009-11-16 22:54:10 +0000

    correction issue 3 : NoSuchElementException dans getPID sur solaris 10 (sans bash ?)

--- trunk/javamelody-core/src/main/java/net/bull/javamelody/PID.java
+++ trunk/javamelody-core/src/main/java/net/bull/javamelody/PID.java
@@ -6,12 +6,13 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.lang.management.ManagementFactory;
+import java.lang.management.RuntimeMXBean;
 import java.util.Locale;
 import java.util.StringTokenizer;
 
 /**
- * @author (getPID) Santhosh Kumar T, http://code.google.com/p/jlibs/, licence LGPL
- * @author (getpids.exe) Daniel Scheibli, http://www.scheibli.com/projects/getpids/index.html, licence GPL
+ * PID du process java.
  */
 final class PID {
 	private PID() {
@@ -24,54 +25,69 @@ final class PID {
 	static String getPID() {
 		String pid = System.getProperty("pid");
 		if (pid == null) {
-			final String[] cmd;
-			File tempFile = null;
-			Process process = null;
-			try {
+			// first, reliable with sun jdk (http://golesny.de/wiki/code:javahowtogetpid)
+			final RuntimeMXBean rtb = ManagementFactory.getRuntimeMXBean();
+			final String processName = rtb.getName();
+			/* tested on: */
+			/* - windows xp sp 2, java 1.5.0_13 */
+			/* - mac os x 10.4.10, java 1.5.0 */
+			/* - debian linux, java 1.5.0_13 */
+			/* all return pid@host, e.g 2204@antonius */
+
+			if (processName.indexOf('@') != -1) {
+				pid = processName.substring(0, processName.indexOf('@'));
+			} else {
+				// following is not always reliable as is (for example, see issue 3 on solaris 10
+				// or http://blog.igorminar.com/2007/03/how-java-application-can-discover-its.html)
+				// Author: Santhosh Kumar T, http://code.google.com/p/jlibs/, licence LGPL
+				// Author getpids.exe: Daniel Scheibli, http://www.scheibli.com/projects/getpids/index.html, licence GPL
+				final String[] cmd;
+				File tempFile = null;
+				Process process = null;
 				try {
-					if (!System.getProperty("os.name").toLowerCase(Locale.getDefault()).contains(
-							"windows")) {
-						cmd = new String[] { "/bin/sh", "-c", "echo $$ $PPID" };
-					} else {
-						// getpids.exe is taken from http://www.scheibli.com/projects/getpids/index.html (GPL)
-						tempFile = File.createTempFile("getpids", ".exe");
+					try {
+						if (!System.getProperty("os.name").toLowerCase(Locale.getDefault())
+								.contains("windows")) {
+							cmd = new String[] { "/bin/sh", "-c", "echo $$ $PPID" };
+						} else {
+							// getpids.exe is taken from http://www.scheibli.com/projects/getpids/index.html (GPL)
+							tempFile = File.createTempFile("getpids", ".exe");
 
-						// extract the embedded getpids.exe file from the jar and save it to above file
-						pump(PID.class.getResourceAsStream("resource/getpids.exe"),
-								new FileOutputStream(tempFile), true, true);
-						cmd = new String[] { tempFile.getAbsolutePath() };
-					}
-					process = Runtime.getRuntime().exec(cmd);
-					final ByteArrayOutputStream bout = new ByteArrayOutputStream();
-					pump(process.getInputStream(), bout, false, true);
+							// extract the embedded getpids.exe file from the jar and save it to above file
+							pump(PID.class.getResourceAsStream("resource/getpids.exe"),
+									new FileOutputStream(tempFile), true, true);
+							cmd = new String[] { tempFile.getAbsolutePath() };
+						}
+						process = Runtime.getRuntime().exec(cmd);
+						final ByteArrayOutputStream bout = new ByteArrayOutputStream();
+						pump(process.getInputStream(), bout, false, true);
 
-					final StringTokenizer stok = new StringTokenizer(bout.toString());
-					stok.nextToken(); // this is pid of the process we spanned
-					pid = stok.nextToken();
-					System.setProperty("pid", pid);
+						final StringTokenizer stok = new StringTokenizer(bout.toString());
+						stok.nextToken(); // this is pid of the process we spanned
+						pid = stok.nextToken();
 
-					// waitFor nécessaire sous windows server 2003
-					// (sinon le fichier temporaire getpidsxxx.exe n'est pas effacé)
-					process.waitFor();
-				} finally {
-					if (process != null) {
-						// évitons http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=6462165
-						process.getInputStream().close();
-						process.getOutputStream().close();
-						process.getErrorStream().close();
-						process.destroy();
+						// waitFor nécessaire sous windows server 2003
+						// (sinon le fichier temporaire getpidsxxx.exe n'est pas effacé)
+						process.waitFor();
+					} finally {
+						if (process != null) {
+							// évitons http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=6462165
+							process.getInputStream().close();
+							process.getOutputStream().close();
+							process.getErrorStream().close();
+							process.destroy();
+						}
+						if (tempFile != null && !tempFile.delete()) {
+							tempFile.deleteOnExit();
+						}
 					}
-					if (tempFile != null && !tempFile.delete()) {
-						tempFile.deleteOnExit();
-					}
+				} catch (final InterruptedException e) {
+					pid = e.toString();
+				} catch (final IOException e) {
+					pid = e.toString();
 				}
-			} catch (final InterruptedException e) {
-				pid = e.toString();
-				System.setProperty("pid", pid);
-			} catch (final IOException e) {
-				pid = e.toString();
-				System.setProperty("pid", pid);
 			}
+			System.setProperty("pid", pid);
 		}
 		return pid;
 	}
