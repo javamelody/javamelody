fe8b28d9548f6dfff91124720791bc24211206a5
evernat@free.fr <evernat@free.fr@80d463d6-a79d-11de-ba6e-69e41c8db662>
2009-11-20 22:39:03 +0000

    mysql : show global status à la place show status car depuis mysql 5.0.2 show status a un scope de session

--- trunk/javamelody-core/src/main/java/net/bull/javamelody/DatabaseInformations.java
+++ trunk/javamelody-core/src/main/java/net/bull/javamelody/DatabaseInformations.java
@@ -59,7 +59,7 @@ class DatabaseInformations implements Serializable {
 						"pg_statio_user_sequences", "pg_settings");
 				break;
 			case MYSQL:
-				tmp = Arrays.asList("processlist", "databases", "variables", "status",
+				tmp = Arrays.asList("processlist", "databases", "variables", "global_status",
 						"innodb_status");
 				break;
 			case ORACLE:
