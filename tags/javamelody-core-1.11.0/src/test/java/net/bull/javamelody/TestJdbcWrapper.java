70079d7081b1917290b189a294e7b97da212659c
evernat@free.fr <evernat@free.fr@80d463d6-a79d-11de-ba6e-69e41c8db662>
2010-01-19 10:25:26 +0000

    [maven-release-plugin]  copy for tag javamelody-core-1.11.0

---
+++ tags/javamelody-core-1.11.0/src/test/java/net/bull/javamelody/TestJdbcWrapper.java
@@ -0,0 +1,250 @@
+/*
+ * Copyright 2008-2009 by Emeric Vernat, Bull
+ *
+ *     This file is part of Java Melody.
+ *
+ * Java Melody is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Java Melody is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Java Melody.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package net.bull.javamelody;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+import org.apache.tomcat.dbcp.dbcp.BasicDataSource;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Test unitaire de la classe JdbcWrapper.
+ * @author Emeric Vernat
+ */
+public class TestJdbcWrapper {
+	static final String H2_DATABASE_URL = "jdbc:h2:~/.h2/test";
+	private JdbcDriver driver;
+	private JdbcWrapper jdbcWrapper;
+
+	/** Test.
+	 * @throws SQLException e */
+	@Before
+	public void setUp() throws SQLException {
+		driver = new JdbcDriver();
+		DriverManager.registerDriver(driver);
+		jdbcWrapper = JdbcWrapper.SINGLETON;
+	}
+
+	/** Test. */
+	@Test
+	public void testRebindDataSources() {
+		// test rebind et stop (sans conteneur)
+		jdbcWrapper.rebindDataSources();
+		jdbcWrapper.stop();
+	}
+
+	/** Test.
+	 * @throws NamingException e */
+	@Test
+	public void testCreateContextProxy() throws NamingException {
+		final Context context = jdbcWrapper.createContextProxy(new InitialContext());
+		assertNotNull("createContextProxy", context);
+		context.close();
+	}
+
+	/** Test.
+	 * @throws SQLException e */
+	@Test
+	public void testCreateDataSourceProxy() throws SQLException {
+		assertTrue("getBasicDataSourceProperties", JdbcWrapper.getBasicDataSourceProperties()
+				.isEmpty());
+		assertTrue("getMaxConnectionCount", JdbcWrapper.getMaxConnectionCount() == -1);
+
+		final org.apache.commons.dbcp.BasicDataSource dbcpDataSource = new org.apache.commons.dbcp.BasicDataSource();
+		dbcpDataSource.setUrl("jdbc:h2:~/.h2/test");
+		dbcpDataSource.setMaxActive(123);
+		final DataSource dbcpProxy = jdbcWrapper.createDataSourceProxy(dbcpDataSource);
+		assertNotNull("createDataSourceProxy", dbcpProxy);
+		assertTrue("getBasicDataSourceProperties", !JdbcWrapper.getBasicDataSourceProperties()
+				.isEmpty());
+		assertTrue("getMaxConnectionCount", JdbcWrapper.getMaxConnectionCount() == 123);
+
+		final BasicDataSource tomcatDataSource = new BasicDataSource();
+		tomcatDataSource.setUrl("jdbc:h2:~/.h2/test");
+		tomcatDataSource.setMaxActive(456);
+		final DataSource tomcatProxy = jdbcWrapper.createDataSourceProxy(tomcatDataSource);
+		assertNotNull("createDataSourceProxy", tomcatProxy);
+		assertNotNull("getLogWriter", tomcatProxy.getLogWriter());
+		tomcatProxy.getConnection().close();
+		assertTrue("getBasicDataSourceProperties", !JdbcWrapper.getBasicDataSourceProperties()
+				.isEmpty());
+		assertTrue("getMaxConnectionCount", JdbcWrapper.getMaxConnectionCount() == 456);
+
+		final DataSource dataSource2 = new DataSource() {
+			/** {@inheritDoc} */
+			public <T> T unwrap(Class<T> iface) throws SQLException {
+				return null;
+			}
+
+			/** {@inheritDoc} */
+			public boolean isWrapperFor(Class<?> iface) throws SQLException {
+				return false;
+			}
+
+			/** {@inheritDoc} */
+			public void setLoginTimeout(int seconds) throws SQLException {
+				tomcatDataSource.setLoginTimeout(seconds);
+			}
+
+			/** {@inheritDoc} */
+			public void setLogWriter(PrintWriter out) throws SQLException {
+				tomcatDataSource.setLogWriter(out);
+			}
+
+			/** {@inheritDoc} */
+			public int getLoginTimeout() throws SQLException {
+				return tomcatDataSource.getLoginTimeout();
+			}
+
+			/** {@inheritDoc} */
+			public PrintWriter getLogWriter() throws SQLException {
+				return tomcatDataSource.getLogWriter();
+			}
+
+			/** {@inheritDoc} */
+			public Connection getConnection(String username, String password) throws SQLException {
+				return tomcatDataSource.getConnection();
+			}
+
+			/** {@inheritDoc} */
+			public Connection getConnection() throws SQLException {
+				return tomcatDataSource.getConnection();
+			}
+		};
+		jdbcWrapper.createDataSourceProxy(dataSource2);
+	}
+
+	/** Test.
+	 * @throws SQLException e 
+	 * @throws IllegalAccessException e */
+	@Test
+	public void testCreateConnectionProxy() throws SQLException, IllegalAccessException {
+		DriverManager.registerDriver(driver);
+		// nécessite la dépendance vers la base de données H2
+		Connection connection = DriverManager.getConnection(H2_DATABASE_URL);
+		try {
+			jdbcWrapper.rewrapConnection(connection);
+			connection = jdbcWrapper.createConnectionProxy(connection);
+			assertNotNull("createConnectionProxy", connection);
+			assertFalse("equals", connection.equals(connection));
+			connection.hashCode();
+
+			connection.prepareStatement("select 1").close();
+			connection.prepareCall("select 1").close();
+
+			connection.rollback();
+
+			assertTrue("proxy of proxy",
+					jdbcWrapper.createConnectionProxy(connection) == connection);
+
+			jdbcWrapper.getSqlCounter().setDisplayed(false);
+			connection = jdbcWrapper.createConnectionProxy(connection);
+			jdbcWrapper.getSqlCounter().setDisplayed(true);
+			System.setProperty(Parameters.PARAMETER_SYSTEM_PREFIX + Parameter.DISABLED.getCode(),
+					"true");
+			try {
+				connection = jdbcWrapper.createConnectionProxy(connection);
+			} finally {
+				System.setProperty(Parameters.PARAMETER_SYSTEM_PREFIX
+						+ Parameter.DISABLED.getCode(), "false");
+			}
+		} finally {
+			connection.close();
+		}
+	}
+
+	/** Test.
+	 * @throws SQLException e
+	 * @throws IllegalAccessException e */
+	@Test
+	public void testRewrapConnection() throws SQLException, IllegalAccessException {
+		DriverManager.registerDriver(driver);
+		// nécessite la dépendance vers la base de données H2
+		final Connection connection = DriverManager.getConnection(H2_DATABASE_URL);
+		jdbcWrapper.rewrapConnection(connection);
+	}
+
+	/** Test.
+	 * @throws SQLException e */
+	@Test
+	public void testStatementProxy() throws SQLException {
+		DriverManager.registerDriver(driver);
+		// nécessite la dépendance vers la base de données H2
+		Connection connection = DriverManager.getConnection(H2_DATABASE_URL);
+		try {
+			connection = jdbcWrapper.createConnectionProxy(connection);
+			final Statement statement = connection.createStatement();
+			try {
+				assertFalse("equals", statement.equals(statement));
+				statement.hashCode();
+
+				statement.executeQuery("select 1").close();
+				statement.execute("select 1");
+				statement.addBatch("select 1");
+				jdbcWrapper.getSqlCounter().setDisplayed(false);
+				statement.execute("select 1");
+				jdbcWrapper.getSqlCounter().setDisplayed(true);
+			} finally {
+				statement.close();
+			}
+		} finally {
+			connection.close();
+		}
+	}
+
+	/** Test. */
+	@Test
+	public void testGetSqlCounter() {
+		assertNotNull("getSqlCounter", jdbcWrapper.getSqlCounter());
+	}
+
+	/** Test. */
+	@Test
+	public void testIsEqualsMethod() {
+		assertTrue("isEqualsMethod", JdbcWrapper.isEqualsMethod("equals", new Object[] { "" }));
+		assertFalse("isEqualsMethod", JdbcWrapper.isEqualsMethod("notequals", new Object[] { "" }));
+		assertFalse("isEqualsMethod", JdbcWrapper.isEqualsMethod("equals", null));
+		assertFalse("isEqualsMethod", JdbcWrapper.isEqualsMethod("equals", new Object[] { "", "" }));
+	}
+
+	/** Test. */
+	@Test
+	public void testIsHashCodeMethod() {
+		assertTrue("isHashCodeMethod", JdbcWrapper.isHashCodeMethod("hashCode", new Object[] {}));
+		assertTrue("isHashCodeMethod", JdbcWrapper.isHashCodeMethod("hashCode", null));
+		assertFalse("isHashCodeMethod", JdbcWrapper
+				.isHashCodeMethod("nothashCode", new Object[] {}));
+		assertFalse("isHashCodeMethod", JdbcWrapper.isHashCodeMethod("hashCode",
+				new Object[] { "" }));
+	}
+}
