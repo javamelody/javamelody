9f5840b2dc9277c733270af2816e1d3a11bf915b
evernat@free.fr <evernat@free.fr@80d463d6-a79d-11de-ba6e-69e41c8db662>
2010-01-20 18:13:05 +0000

    [maven-release-plugin]  copy for tag javamelody-core-1.11.1

---
+++ tags/javamelody-core-1.11.1/src/main/java/net/bull/javamelody/HtmlProcessInformationsReport.java
@@ -0,0 +1,138 @@
+/*
+ * Copyright 2008-2009 by Emeric Vernat, Bull
+ *
+ *     This file is part of Java Melody.
+ *
+ * Java Melody is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Java Melody is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Java Melody.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package net.bull.javamelody;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.text.DecimalFormat;
+import java.util.List;
+
+/**
+ * Partie du rapport html pour les processus du système d'exploitation.
+ * @author Emeric Vernat
+ */
+class HtmlProcessInformationsReport {
+	private final List<ProcessInformations> processInformationsList;
+	private final Writer writer;
+	private final DecimalFormat percentFormat = I18N.createPercentFormat();
+	private final DecimalFormat integerFormat = I18N.createIntegerFormat();
+
+	HtmlProcessInformationsReport(List<ProcessInformations> processInformationsList, Writer writer) {
+		super();
+		assert processInformationsList != null;
+		assert writer != null;
+
+		this.processInformationsList = processInformationsList;
+		this.writer = writer;
+	}
+
+	void toHtml() throws IOException {
+		writeLinks();
+		writeln("<br/>");
+
+		writeln("<img src='?resource=threads.png' width='24' height='24' alt='#Processus#' />&nbsp;");
+		writeln("<b>#Processus#</b>");
+		writeTable();
+	}
+
+	void writeTable() throws IOException {
+		writeln("<table class='sortable' width='100%' border='1' cellspacing='0' cellpadding='2' summary='#Processus#'>");
+		write("<thead><tr><th>#Utilisateur#</th>");
+		write("<th class='sorttable_numeric'>#PID#</th>");
+		if (!ProcessInformations.WINDOWS) {
+			write("<th class='sorttable_numeric'>#cpu#</th><th class='sorttable_numeric'>#mem#</th>");
+		}
+		write("<th class='sorttable_numeric'>#vsz#</th>");
+		if (!ProcessInformations.WINDOWS) {
+			write("<th class='sorttable_numeric'>#rss#</th><th>#tty#</th>");
+			write("<th>#stat#</th><th>#start#</th>");
+		}
+		write("<th>#cpuTime#</th><th>#command#</th>");
+		writeln("</tr></thead><tbody>");
+		boolean odd = false;
+		for (final ProcessInformations processInformations : processInformationsList) {
+			if (odd) {
+				write("<tr class='odd' onmouseover=\"this.className='highlight'\" onmouseout=\"this.className='odd'\">");
+			} else {
+				write("<tr onmouseover=\"this.className='highlight'\" onmouseout=\"this.className=''\">");
+			}
+			odd = !odd; // NOPMD
+			writeProcessInformations(processInformations);
+			writeln("</tr>");
+		}
+		writeln("</tbody></table>");
+		if (!ProcessInformations.WINDOWS) {
+			write("<div align='right'>");
+			write("<a href='http://fr.wikipedia.org/wiki/Ps_(Unix)' target='_blank'>ps command reference</a></div>");
+		}
+	}
+
+	private void writeProcessInformations(ProcessInformations processInformations)
+			throws IOException {
+		write("<td>");
+		write(htmlEncode(processInformations.getUser()));
+		final String newColumnRight = "</td><td align='right'>";
+		final String newColumn = "</td><td>";
+		write(newColumnRight);
+		write(integerFormat.format(processInformations.getPid()));
+		if (!ProcessInformations.WINDOWS) {
+			write(newColumnRight);
+			write(percentFormat.format(processInformations.getCpuPercentage()));
+			write(newColumnRight);
+			write(percentFormat.format(processInformations.getMemPercentage()));
+		}
+		write(newColumnRight);
+		write(integerFormat.format(processInformations.getVsz()));
+		if (!ProcessInformations.WINDOWS) {
+			write(newColumnRight);
+			write(integerFormat.format(processInformations.getRss()));
+			write(newColumn);
+			write(htmlEncode(processInformations.getTty()));
+			write(newColumn);
+			write(htmlEncode(processInformations.getStat()));
+			write(newColumnRight);
+			write(htmlEncode(processInformations.getStart()));
+		}
+		write(newColumnRight);
+		write(htmlEncode(processInformations.getCpuTime()));
+		write(newColumn);
+		write(htmlEncode(processInformations.getCommand()));
+		write("</td>");
+	}
+
+	private void writeLinks() throws IOException {
+		writeln("<div class='noPrint'>");
+		writeln("<a href='javascript:history.back()'><img src='?resource=action_back.png' alt='#Retour#'/> #Retour#</a>");
+		writeln("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;");
+		writeln("<a href='?part=processes'><img src='?resource=action_refresh.png' alt='#Actualiser#'/> #Actualiser#</a>");
+		writeln("</div>");
+	}
+
+	private static String htmlEncode(String text) {
+		return I18N.htmlEncode(text, true);
+	}
+
+	private void write(String html) throws IOException {
+		I18N.writeTo(html, writer);
+	}
+
+	private void writeln(String html) throws IOException {
+		I18N.writelnTo(html, writer);
+	}
+}
---
+++ tags/javamelody-core-1.11.1/src/main/java/net/bull/javamelody/PdfCacheInformationsReport.java
@@ -0,0 +1,142 @@
+/*
+ * Copyright 2008-2009 by Emeric Vernat, Bull
+ *
+ *     This file is part of Java Melody.
+ *
+ * Java Melody is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Java Melody is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Java Melody.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package net.bull.javamelody;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import com.lowagie.text.Anchor;
+import com.lowagie.text.Document;
+import com.lowagie.text.DocumentException;
+import com.lowagie.text.Element;
+import com.lowagie.text.Font;
+import com.lowagie.text.Paragraph;
+import com.lowagie.text.Phrase;
+import com.lowagie.text.pdf.PdfPCell;
+import com.lowagie.text.pdf.PdfPTable;
+
+/**
+ * Partie du rapport pdf pour le cache de données.
+ * @author Emeric Vernat
+ */
+class PdfCacheInformationsReport {
+	private final List<CacheInformations> cacheInformationsList;
+	private final Document document;
+	private final DecimalFormat integerFormat = I18N.createIntegerFormat();
+	private final Font cellFont = PdfDocumentFactory.TABLE_CELL_FONT;
+	private PdfPTable currentTable;
+	private final boolean hitsRatioEnabled;
+	private final boolean configurationEnabled;
+
+	PdfCacheInformationsReport(List<CacheInformations> cacheInformationsList, Document document) {
+		super();
+		assert cacheInformationsList != null;
+		assert document != null;
+
+		this.cacheInformationsList = cacheInformationsList;
+		this.document = document;
+		this.hitsRatioEnabled = HtmlCacheInformationsReport
+				.isHitsRatioEnabled(cacheInformationsList);
+		this.configurationEnabled = HtmlCacheInformationsReport
+				.isConfigurationEnabled(cacheInformationsList);
+	}
+
+	void toPdf() throws DocumentException {
+		writeHeader();
+
+		final PdfPCell defaultCell = getDefaultCell();
+		boolean odd = false;
+		for (final CacheInformations cacheInformations : cacheInformationsList) {
+			if (odd) {
+				defaultCell.setGrayFill(0.97f);
+			} else {
+				defaultCell.setGrayFill(1);
+			}
+			odd = !odd; // NOPMD
+			writeCacheInformations(cacheInformations);
+		}
+		document.add(currentTable);
+		final Anchor anchor = new Anchor("Configuration reference", PdfDocumentFactory.BLUE_FONT);
+		anchor.setName("Ehcache configuration reference");
+		anchor
+				.setReference("http://ehcache.sourceforge.net/apidocs/net/sf/ehcache/config/CacheConfiguration.html#field_summary");
+		anchor.setFont(PdfDocumentFactory.BLUE_FONT);
+		final Paragraph paragraph = new Paragraph();
+		paragraph.add(anchor);
+		paragraph.setAlignment(Element.ALIGN_RIGHT);
+		document.add(paragraph);
+	}
+
+	private void writeHeader() throws DocumentException {
+		final List<String> headers = createHeaders();
+		final int[] relativeWidths = new int[headers.size()];
+		Arrays.fill(relativeWidths, 0, headers.size(), 1);
+		if (configurationEnabled) {
+			relativeWidths[headers.size() - 1] = 4;
+		}
+
+		currentTable = PdfDocumentFactory.createPdfPTable(headers, relativeWidths);
+	}
+
+	private List<String> createHeaders() {
+		final List<String> headers = new ArrayList<String>();
+		headers.add(getI18nString("Cache"));
+		headers.add(getI18nString("Nb_objets_en_memoire"));
+		headers.add(getI18nString("Nb_objets_sur_disque"));
+		if (hitsRatioEnabled) {
+			headers.add(getI18nString("Efficacite_cache_memoire"));
+			headers.add(getI18nString("Efficacite_cache"));
+		}
+		if (configurationEnabled) {
+			headers.add(getI18nString("Configuration"));
+		}
+		return headers;
+	}
+
+	private void writeCacheInformations(CacheInformations cacheInformations) {
+		final PdfPCell defaultCell = getDefaultCell();
+		defaultCell.setHorizontalAlignment(Element.ALIGN_LEFT);
+		addCell(cacheInformations.getName());
+		defaultCell.setHorizontalAlignment(Element.ALIGN_RIGHT);
+		addCell(integerFormat.format(cacheInformations.getInMemoryObjectCount()));
+		addCell(integerFormat.format(cacheInformations.getOnDiskObjectCount()));
+		if (hitsRatioEnabled) {
+			addCell(integerFormat.format(cacheInformations.getInMemoryHitsRatio()));
+			addCell(integerFormat.format(cacheInformations.getHitsRatio()));
+		}
+		if (configurationEnabled) {
+			defaultCell.setHorizontalAlignment(Element.ALIGN_LEFT);
+			addCell(cacheInformations.getConfiguration());
+		}
+	}
+
+	private static String getI18nString(String key) {
+		return I18N.getString(key);
+	}
+
+	private PdfPCell getDefaultCell() {
+		return currentTable.getDefaultCell();
+	}
+
+	private void addCell(String string) {
+		currentTable.addCell(new Phrase(string, cellFont));
+	}
+}
