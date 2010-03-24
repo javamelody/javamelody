1a830baeb421747d45284164e6c1fd93efb7b683
evernat <evernat@free.fr>
2010-03-24 11:02:47 +0000

    correction TU quand il est exécuté à certaines heures

--- javamelody-core/src/test/java/net/bull/javamelody/TestRange.java
+++ javamelody-core/src/test/java/net/bull/javamelody/TestRange.java
@@ -38,6 +38,7 @@ import org.junit.Test;
 public class TestRange {
 	private static final long ONE_DAY_SECONDS = 24 * 60 * 60;
 	private static final long ONE_DAY_MILLIS = ONE_DAY_SECONDS * 1000;
+	private static final long ONE_MINUTE_MILLIS = 60 * 1000;
 	private Range periodRange;
 	private Range customRange;
 
@@ -80,17 +81,15 @@ public class TestRange {
 	/** Test. */
 	@Test
 	public void testParse() {
-		assertEquals("parse1", periodRange.getPeriod(), Range.parse(periodRange.getValue())
-				.getPeriod());
-		assertEquals("parse2", customRange.getStartDate().getTime() / ONE_DAY_MILLIS, Range.parse(
-				customRange.getValue()).getStartDate().getTime()
-				/ ONE_DAY_MILLIS + 1);
-		assertEquals("parse3", customRange.getEndDate().getTime() / ONE_DAY_MILLIS, Range.parse(
-				customRange.getValue()).getEndDate().getTime()
-				/ ONE_DAY_MILLIS);
-
 		I18N.bindLocale(Locale.FRENCH);
 		try {
+			assertEquals("parse1", periodRange.getPeriod(), Range.parse(periodRange.getValue())
+					.getPeriod());
+			assertTrue("parse2", isSameDay(customRange.getStartDate(), Range.parse(
+					customRange.getValue()).getStartDate()));
+			assertTrue("parse3", isSameDay(customRange.getEndDate(), Range.parse(
+					customRange.getValue()).getEndDate()));
+
 			// on teste le résultat en cas d'erreur de format
 			assertNotNull("parse4", Range.parse("xxxxxx-01/01/2010"));
 			assertNotNull("parse5", Range.parse("01/01/2010-xxxxxx"));
@@ -109,6 +108,13 @@ public class TestRange {
 		} finally {
 			I18N.unbindLocale();
 		}
+	}
+
+	@SuppressWarnings("deprecation")
+	private static boolean isSameDay(Date date1, Date date2) {
+		return (date1.getTime() - date1.getTimezoneOffset() * ONE_MINUTE_MILLIS) / ONE_DAY_MILLIS == (date2
+				.getTime() - date2.getTimezoneOffset() * ONE_MINUTE_MILLIS)
+				/ ONE_DAY_MILLIS;
 	}
 
 	/** Test. */
