620f9d5caba7d187aa64d3ae895a1c47625f9166
evernat <evernat@free.fr>
2010-06-18 18:51:22 +0000

    refactoring

--- javamelody-core/src/main/java/net/bull/javamelody/PID.java
+++ javamelody-core/src/main/java/net/bull/javamelody/PID.java
@@ -37,57 +37,63 @@ final class PID {
 			if (processName.indexOf('@') != -1) {
 				pid = processName.substring(0, processName.indexOf('@'));
 			} else {
-				// following is not always reliable as is (for example, see issue 3 on solaris 10
-				// or http://blog.igorminar.com/2007/03/how-java-application-can-discover-its.html)
-				// Author: Santhosh Kumar T, http://code.google.com/p/jlibs/, licence LGPL
-				// Author getpids.exe: Daniel Scheibli, http://www.scheibli.com/projects/getpids/index.html, licence GPL
-				final String[] cmd;
-				File tempFile = null;
-				Process process = null;
-				try {
-					try {
-						if (!System.getProperty("os.name").toLowerCase(Locale.getDefault())
-								.contains("windows")) {
-							cmd = new String[] { "/bin/sh", "-c", "echo $$ $PPID" };
-						} else {
-							// getpids.exe is taken from http://www.scheibli.com/projects/getpids/index.html (GPL)
-							tempFile = File.createTempFile("getpids", ".exe");
-
-							// extract the embedded getpids.exe file from the jar and save it to above file
-							pump(PID.class.getResourceAsStream("resource/getpids.exe"),
-									new FileOutputStream(tempFile), true, true);
-							cmd = new String[] { tempFile.getAbsolutePath() };
-						}
-						process = Runtime.getRuntime().exec(cmd);
-						final ByteArrayOutputStream bout = new ByteArrayOutputStream();
-						pump(process.getInputStream(), bout, false, true);
-
-						final StringTokenizer stok = new StringTokenizer(bout.toString());
-						stok.nextToken(); // this is pid of the process we spanned
-						pid = stok.nextToken();
-
-						// waitFor nécessaire sous windows server 2003
-						// (sinon le fichier temporaire getpidsxxx.exe n'est pas effacé)
-						process.waitFor();
-					} finally {
-						if (process != null) {
-							// évitons http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=6462165
-							process.getInputStream().close();
-							process.getOutputStream().close();
-							process.getErrorStream().close();
-							process.destroy();
-						}
-						if (tempFile != null && !tempFile.delete()) {
-							tempFile.deleteOnExit();
-						}
-					}
-				} catch (final InterruptedException e) {
-					pid = e.toString();
-				} catch (final IOException e) {
-					pid = e.toString();
-				}
+				pid = getPIDFromOS();
 			}
 			System.setProperty("pid", pid);
+		}
+		return pid;
+	}
+
+	static String getPIDFromOS() {
+		String pid;
+		// following is not always reliable as is (for example, see issue 3 on solaris 10
+		// or http://blog.igorminar.com/2007/03/how-java-application-can-discover-its.html)
+		// Author: Santhosh Kumar T, http://code.google.com/p/jlibs/, licence LGPL
+		// Author getpids.exe: Daniel Scheibli, http://www.scheibli.com/projects/getpids/index.html, licence GPL
+		final String[] cmd;
+		File tempFile = null;
+		Process process = null;
+		try {
+			try {
+				if (!System.getProperty("os.name").toLowerCase(Locale.getDefault()).contains(
+						"windows")) {
+					cmd = new String[] { "/bin/sh", "-c", "echo $$ $PPID" };
+				} else {
+					// getpids.exe is taken from http://www.scheibli.com/projects/getpids/index.html (GPL)
+					tempFile = File.createTempFile("getpids", ".exe");
+
+					// extract the embedded getpids.exe file from the jar and save it to above file
+					pump(PID.class.getResourceAsStream("resource/getpids.exe"),
+							new FileOutputStream(tempFile), true, true);
+					cmd = new String[] { tempFile.getAbsolutePath() };
+				}
+				process = Runtime.getRuntime().exec(cmd);
+				final ByteArrayOutputStream bout = new ByteArrayOutputStream();
+				pump(process.getInputStream(), bout, false, true);
+
+				final StringTokenizer stok = new StringTokenizer(bout.toString());
+				stok.nextToken(); // this is pid of the process we spanned
+				pid = stok.nextToken();
+
+				// waitFor nécessaire sous windows server 2003
+				// (sinon le fichier temporaire getpidsxxx.exe n'est pas effacé)
+				process.waitFor();
+			} finally {
+				if (process != null) {
+					// évitons http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=6462165
+					process.getInputStream().close();
+					process.getOutputStream().close();
+					process.getErrorStream().close();
+					process.destroy();
+				}
+				if (tempFile != null && !tempFile.delete()) {
+					tempFile.deleteOnExit();
+				}
+			}
+		} catch (final InterruptedException e) {
+			pid = e.toString();
+		} catch (final IOException e) {
+			pid = e.toString();
 		}
 		return pid;
 	}
