1ba7487b616ab10db16d13c91bcd9341e0a5e9fb
evernat@free.fr <evernat@free.fr@80d463d6-a79d-11de-ba6e-69e41c8db662>
2010-08-24 19:13:20 +0000

    compléments tests unitaires

--- trunk/javamelody-core/src/test/java/net/bull/javamelody/TestCollector.java
+++ trunk/javamelody-core/src/test/java/net/bull/javamelody/TestCollector.java
@@ -25,6 +25,7 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URL;
 import java.util.Arrays;
@@ -226,6 +227,43 @@ public class TestCollector {
 				robin.deleteFile();
 			}
 			assertNull("getJRobin null", collector.getJRobin("n'importe quoi"));
+		} finally {
+			timer.cancel();
+		}
+	}
+
+	/** Test.
+	 * @throws IOException e */
+	@Test
+	public void testJRobinResetFile() throws IOException {
+		try {
+			final String application = TEST;
+			final String jrobinName = "name";
+			final File dir = Parameters.getStorageDirectory(application);
+			// ce fichier sera celui utilisé par JRobin
+			final File rrdFile = new File(dir, jrobinName + ".rrd");
+			assertTrue("delete", !rrdFile.exists() || rrdFile.delete());
+			final FileOutputStream out = new FileOutputStream(rrdFile);
+			try {
+				// il faut un minimum de quantité de données pour avoir RrdException "Invalid file header"
+				// et non une IOException dans NIO
+				for (int i = 0; i < 100; i++) {
+					out.write("n'est pas un fichier rrd".getBytes());
+				}
+			} finally {
+				out.close();
+			}
+			final JRobin jrobin = JRobin.createInstance(application, jrobinName, "request");
+			// addValue devrait appeler resetFile car RrdException "Invalid file header"
+			// puis devrait relancer une IOException
+			try {
+				jrobin.addValue(1);
+			} catch (final IOException e) {
+				assertTrue("cause", e.getCause() != null && e.getCause().getMessage() != null
+						&& e.getCause().getMessage().contains("Invalid file header"));
+			}
+			// après ce resetFile, on devrait pouvoir appeler addValue
+			jrobin.addValue(1);
 		} finally {
 			timer.cancel();
 		}
