fb798fe758fc1df3f8678971d07f9df21a51a320
evernat@free.fr <evernat@free.fr@80d463d6-a79d-11de-ba6e-69e41c8db662>
2010-10-23 17:26:17 +0000

    compléments tests unitaires

--- trunk/javamelody-core/src/test/java/net/bull/javamelody/TestTomcatInformations.java
+++ trunk/javamelody-core/src/test/java/net/bull/javamelody/TestTomcatInformations.java
@@ -2,7 +2,7 @@
  * Copyright 2008-2010 by Emeric Vernat
  *
  *     This file is part of Java Melody.
- *fv
+ *
  * Java Melody is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
