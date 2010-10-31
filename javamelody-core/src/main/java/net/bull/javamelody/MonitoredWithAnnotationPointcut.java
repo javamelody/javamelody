eae0983a57398f26c16b2d616ac5f5485a147f3b
evernat <evernat@free.fr>
2010-10-31 16:57:37 +0000

    l'annotation MonitoredWithSpring peut être déclarée sur la classe d'interface comme sur la classe d'implémentation

--- javamelody-core/src/main/java/net/bull/javamelody/MonitoredWithAnnotationPointcut.java
+++ javamelody-core/src/main/java/net/bull/javamelody/MonitoredWithAnnotationPointcut.java
@@ -34,6 +34,7 @@ public class MonitoredWithAnnotationPointcut implements Pointcut {
 		@SuppressWarnings({ "unchecked", "rawtypes" })
 		public boolean matches(Method method, Class targetClass) {
 			return targetClass.isAnnotationPresent(MonitoredWithSpring.class)
+					|| method.getDeclaringClass().isAnnotationPresent(MonitoredWithSpring.class)
 					|| method.isAnnotationPresent(MonitoredWithSpring.class);
 		}
 
