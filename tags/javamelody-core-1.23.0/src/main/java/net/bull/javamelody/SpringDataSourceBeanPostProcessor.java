b2153a694d9127e25bdb56fd7c53120af206d02f
evernat@free.fr <evernat@free.fr@80d463d6-a79d-11de-ba6e-69e41c8db662>
2010-11-27 16:30:43 +0000

    [maven-release-plugin]  copy for tag javamelody-core-1.23.0

---
+++ tags/javamelody-core-1.23.0/src/main/java/net/bull/javamelody/SpringDataSourceBeanPostProcessor.java
@@ -0,0 +1,132 @@
+/*
+ * Copyright 2008-2010 by Emeric Vernat
+ *
+ *     This file is part of Java Melody.
+ *
+ * Java Melody is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Java Melody is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Java Melody.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package net.bull.javamelody;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.util.Set;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.jndi.JndiObjectFactoryBean;
+
+/**
+ * Post-processor Spring pour une éventuelle DataSource défini dans le fichier xml Spring.
+ * @author Emeric Vernat
+ */
+public class SpringDataSourceBeanPostProcessor implements BeanPostProcessor {
+	private Set<String> excludedDatasources;
+
+	/**
+	 * Définit les noms des datasources Spring exclues.
+	 * @param excludedDatasources Set
+	 */
+	public void setExcludedDatasources(Set<String> excludedDatasources) {
+		this.excludedDatasources = excludedDatasources;
+
+		// exemple:
+		//	<bean id="springDataSourceBeanPostProcessor" class="net.bull.javamelody.SpringDataSourceBeanPostProcessor">
+		//		<property name="excludedDatasources">
+		//			<set>
+		//				<value>excludedDataSourceName</value>
+		//			</set>
+		//		</property>
+		// 	</bean>
+	}
+
+	/** {@inheritDoc} */
+	public Object postProcessBeforeInitialization(Object bean, String beanName) {
+		return bean;
+	}
+
+	/** {@inheritDoc} */
+	public Object postProcessAfterInitialization(final Object bean, final String beanName) {
+		if (excludedDatasources != null && excludedDatasources.contains(beanName)) {
+			LOG.debug("Spring datasource excluded: " + beanName);
+			return bean;
+		}
+		if (bean instanceof DataSource && !Parameters.isNoDatabase()) {
+			final DataSource dataSource = (DataSource) bean;
+			JdbcWrapperHelper.registerSpringDataSource(beanName, dataSource);
+			final DataSource result = JdbcWrapper.SINGLETON.createDataSourceProxy(beanName,
+					dataSource);
+			LOG.debug("Spring datasource wrapped: " + beanName);
+			return result;
+		} else if (bean instanceof JndiObjectFactoryBean && !Parameters.isNoDatabase()) {
+			// fix issue 20
+			final InvocationHandler invocationHandler = new InvocationHandler() {
+				/** {@inheritDoc} */
+				public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+					Object result = method.invoke(bean, args);
+					if (result instanceof DataSource) {
+						result = JdbcWrapper.SINGLETON.createDataSourceProxy(beanName,
+								(DataSource) result);
+					}
+					return result;
+				}
+			};
+			final Object result = JdbcWrapper.createProxy(bean, invocationHandler);
+			LOG.debug("Spring JNDI factory wrapped: " + beanName);
+			return result;
+		}
+
+		// I tried here in the post-processor to fix "quartz jobs which are scheduled with spring
+		// are not displayed in javamelody, except if there is the following property for
+		// SchedulerFactoryBean in spring xml:
+		// <property name="exposeSchedulerInRepository" value="true" /> ",
+
+		// but I had some problem with Spring creating the scheduler
+		// twice and so registering the scheduler in SchedulerRepository with the same name
+		// as the one registered below (and Quartz wants not)
+		//		else if (bean != null
+		//				&& "org.springframework.scheduling.quartz.SchedulerFactoryBean".equals(bean
+		//						.getClass().getName())) {
+		//			try {
+		//				// Remarque: on ajoute nous même le scheduler de Spring dans le SchedulerRepository
+		//				// de Quartz, car l'appel ici de schedulerFactoryBean.setExposeSchedulerInRepository(true)
+		//				// est trop tard et ne fonctionnerait pas
+		//				final Method method = bean.getClass().getMethod("getScheduler", (Class<?>[]) null);
+		//				final Scheduler scheduler = (Scheduler) method.invoke(bean, (Object[]) null);
+		//
+		//				final SchedulerRepository schedulerRepository = SchedulerRepository.getInstance();
+		//				synchronized (schedulerRepository) {
+		//					if (schedulerRepository.lookup(scheduler.getSchedulerName()) == null) {
+		//						schedulerRepository.bind(scheduler);
+		//						scheduler.addGlobalJobListener(new JobGlobalListener());
+		//					}
+		//				}
+		//			} catch (final NoSuchMethodException e) {
+		//				// si la méthode n'existe pas (avant spring 2.5.6), alors cela marche sans rien faire
+		//				return bean;
+		//			} catch (final InvocationTargetException e) {
+		//				// tant pis
+		//				return bean;
+		//			} catch (final IllegalAccessException e) {
+		//				// tant pis
+		//				return bean;
+		//			} catch (SchedulerException e) {
+		//				// tant pis
+		//				return bean;
+		//			}
+		//		}
+
+		return bean;
+	}
+}
---
+++ tags/javamelody-core-1.23.0/src/test/java/net/bull/javamelody/TestTomcatInformations.java
@@ -0,0 +1,206 @@
+/*
+ * Copyright 2008-2010 by Emeric Vernat
+ *
+ *     This file is part of Java Melody.
+ *
+ * Java Melody is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Java Melody is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Java Melody.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package net.bull.javamelody;
+
+import static org.junit.Assert.assertNotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.management.JMException;
+import javax.management.MBeanServer;
+import javax.management.MBeanServerFactory;
+import javax.management.ObjectName;
+
+import org.junit.Test;
+
+/**
+ * Test unitaire de la classe TomcatInformations.
+ * @author Emeric Vernat
+ */
+public class TestTomcatInformations {
+	/**
+	 * Implémentation du MBean ThreadPool.
+	 * @author Emeric Vernat
+	 */
+	public static class ThreadPool implements ThreadPoolMBean {
+		/** {@inheritDoc} */
+		public int getmaxThreads() {
+			return 200;
+		}
+
+		/** {@inheritDoc} */
+		public int getcurrentThreadsBusy() {
+			return 22;
+		}
+
+		/** {@inheritDoc} */
+		public int getcurrentThreadCount() {
+			return 42;
+		}
+	}
+
+	/**
+	 * Interface du MBean ThreadPool.
+	 * @author Emeric Vernat
+	 */
+	public interface ThreadPoolMBean {
+		/**
+		 * attribut maxThreads.
+		 * @return int
+		 */
+		int getmaxThreads();
+
+		/**
+		 * attribut currentThreadsBusy.
+		 * @return int
+		 */
+		int getcurrentThreadsBusy();
+
+		/**
+		 * attribut currentThreadCount.
+		 * @return int
+		 */
+		int getcurrentThreadCount();
+	}
+
+	/**
+	 * Implémentation du MBean GlobalRequestProcessor.
+	 * @author Emeric Vernat
+	 */
+	public static class GlobalRequestProcessor implements GlobalRequestProcessorMBean {
+		private int requestCount = 100;
+
+		/** {@inheritDoc} */
+		public long getbytesReceived() {
+			return 0;
+		}
+
+		/** {@inheritDoc} */
+		public long getbytesSent() {
+			return 100000;
+		}
+
+		/** {@inheritDoc} */
+		public int getrequestCount() {
+			return requestCount;
+		}
+
+		/**
+		 * setter.
+		 * @param count int
+		 */
+		public void setrequestCount(int count) {
+			this.requestCount = count;
+		}
+
+		/** {@inheritDoc} */
+		public int geterrorCount() {
+			return 1;
+		}
+
+		/** {@inheritDoc} */
+		public long getprocessingTime() {
+			return 2000;
+		}
+
+		/** {@inheritDoc} */
+		public long getmaxTime() {
+			return 10000;
+		}
+	}
+
+	/**
+	 * Interface du MBean GlobalRequestProcessor.
+	 * @author Emeric Vernat
+	 */
+	public interface GlobalRequestProcessorMBean {
+		/**
+		 * attribut bytesReceived.
+		 * @return int
+		 */
+		long getbytesReceived();
+
+		/**
+		 * attribut bytesSent.
+		 * @return int
+		 */
+		long getbytesSent();
+
+		/**
+		 * attribut requestCount.
+		 * @return int
+		 */
+		int getrequestCount();
+
+		/**
+		 * attribut errorCount.
+		 * @return int
+		 */
+		int geterrorCount();
+
+		/**
+		 * attribut processingTime.
+		 * @return int
+		 */
+		long getprocessingTime();
+
+		/**
+		 * attribut maxTime.
+		 * @return int
+		 */
+		long getmaxTime();
+	}
+
+	/** Test.
+	 * @throws JMException e */
+	@Test
+	public void testTomcatInformations() throws JMException {
+		System.setProperty("catalina.home", "unknown");
+		// ce premier appel crée un MBeanServer
+		TomcatInformations.initMBeans();
+		assertNotNull("buildTomcatInformationsList",
+				TomcatInformations.buildTomcatInformationsList());
+		final MBeanServer mBeanServer = MBeanServerFactory.findMBeanServer(null).get(0);
+		final List<ObjectName> mBeans = new ArrayList<ObjectName>();
+		mBeans.add(mBeanServer.registerMBean(new ThreadPool(),
+				new ObjectName("Catalina:type=ThreadPool,name=http-8080")).getObjectName());
+		TomcatInformations.initMBeans();
+		try {
+			// les appels suivants réutilise le MBeanServer créé
+			assertNotNull("buildTomcatInformationsList",
+					TomcatInformations.buildTomcatInformationsList());
+			mBeans.add(mBeanServer.registerMBean(new GlobalRequestProcessor(),
+					new ObjectName("Catalina:type=GlobalRequestProcessor,name=http-8080"))
+					.getObjectName());
+			TomcatInformations.initMBeans();
+			assertNotNull("buildTomcatInformationsList",
+					TomcatInformations.buildTomcatInformationsList());
+			for (final TomcatInformations tomcatInformations : TomcatInformations
+					.buildTomcatInformationsList()) {
+				tomcatInformations.toString();
+			}
+		} finally {
+			for (final ObjectName registeredMBean : mBeans) {
+				mBeanServer.unregisterMBean(registeredMBean);
+			}
+			TomcatInformations.initMBeans();
+		}
+	}
+}
