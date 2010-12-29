7adfdad3ff8cd86efb0214fb496da0ec262413f6
evernat@free.fr <evernat@free.fr@80d463d6-a79d-11de-ba6e-69e41c8db662>
2010-12-29 00:38:26 +0000

    added note on license

--- trunk/javamelody-core/src/main/java/net/bull/javamelody/HeapHistogram.java
+++ trunk/javamelody-core/src/main/java/net/bull/javamelody/HeapHistogram.java
@@ -42,6 +42,18 @@ import java.util.Scanner;
  * Adaptée uniquement pour le style de code, le comparateur, la méthode add et la source.
  * @author Tomas Hurka
  */
+// Added note: As said above, this "particular file" is said to be GPL+classpath exception:
+// http://openjdk.java.net/legal/gplv2+ce.html
+//
+// Extract: "As a special exception, the copyright holders of this library give you
+// permission to link this library with independent modules to produce an
+// executable, regardless of the license terms of these independent modules,
+// and to copy and distribute the resulting executable under terms of your
+// choice, provided that you also meet, for each linked independent module,
+// the terms and conditions of the license of that module."
+//
+// And so, I understand that the whole module here can use LGPL, except this file which uses GPL+CE.
+
 class HeapHistogram implements Serializable {
 	private static final long serialVersionUID = 2163916067335213382L;
 	private static final String BOOLEAN_TEXT = "boolean"; // NOI18N
