16e114a52859b0c5a9fd00e85b1c046410c5aa50
evernat <evernat@free.fr>
2011-03-23 18:15:01 +0000

    change author as request by the author and add the forgotten file header

--- javamelody-core/src/main/java/net/bull/javamelody/SpringDataSourceFactoryBean.java
+++ javamelody-core/src/main/java/net/bull/javamelody/SpringDataSourceFactoryBean.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright 2008-2010 by Emeric Vernat
+ *
+ *     This file is part of Java Melody.
+ *
+ * Java Melody is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Java Melody is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Java Melody.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package net.bull.javamelody;
 
 import javax.sql.DataSource;
@@ -11,7 +29,7 @@ import org.springframework.beans.factory.config.AbstractFactoryBean;
  * @see AbstractFactoryBean
  * @see JdbcWrapper
  *
- * @author et2448, Emeric Vernat
+ * @author David J. M. Karlsen (davidkarlsen at gmail.com), Emeric Vernat
  */
 public class SpringDataSourceFactoryBean extends AbstractFactoryBean {
 	private String targetName;
