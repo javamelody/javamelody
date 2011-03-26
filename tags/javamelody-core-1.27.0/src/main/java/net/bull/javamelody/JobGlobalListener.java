ab1365f0a7531f3c74b374752dfc9b2b4c207229
evernat@free.fr <evernat@free.fr@80d463d6-a79d-11de-ba6e-69e41c8db662>
2011-03-26 22:35:43 +0000

    [maven-release-plugin]  copy for tag javamelody-core-1.27.0

---
+++ tags/javamelody-core-1.27.0/src/main/java/net/bull/javamelody/JobGlobalListener.java
@@ -0,0 +1,133 @@
+/*
+ * Copyright 2008-2010 by Emeric Vernat
+ *
+ *     This file is part of Java Melody.
+ *
+ * Java Melody is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Java Melody is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Java Melody.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package net.bull.javamelody;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.quartz.JobDetail;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.quartz.JobListener;
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+import org.quartz.impl.StdSchedulerFactory;
+
+/**
+ * Listener sur les exécutions de jobs quartz, configuré automatiquement par MonitoringFilter.
+ * @author Emeric Vernat
+ */
+final class JobGlobalListener implements JobListener {
+	private static final Counter JOB_COUNTER = new Counter(Counter.JOB_COUNTER_NAME, "jobs.png",
+			JdbcWrapper.SINGLETON.getSqlCounter());
+
+	static Counter getJobCounter() {
+		return JOB_COUNTER;
+	}
+
+	static void initJobGlobalListener() {
+		try {
+			final JobGlobalListener jobGlobalListener = new JobGlobalListener();
+			final Scheduler defaultScheduler;
+			if (Boolean.parseBoolean(Parameters
+					.getParameter(Parameter.QUARTZ_DEFAULT_LISTENER_DISABLED))) {
+				defaultScheduler = null;
+			} else {
+				defaultScheduler = StdSchedulerFactory.getDefaultScheduler();
+				defaultScheduler.addGlobalJobListener(jobGlobalListener);
+			}
+			for (final Scheduler scheduler : JobInformations.getAllSchedulers()) {
+				if (scheduler != defaultScheduler) {
+					scheduler.addGlobalJobListener(jobGlobalListener);
+				}
+			}
+			LOG.debug("job global listener initialized");
+		} catch (final SchedulerException e) {
+			// initialisation du JobGlobalListener échouée, tant pis, il n'y aura pas les temps pour quartz
+			// (cela peut arriver, en particulier en développement dans Grails)
+			LOG.info("initialization of job global listener failed, skipping", e);
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	static void destroyJobGlobalListener() {
+		try {
+			for (final Scheduler scheduler : JobInformations.getAllSchedulers()) {
+				final List<JobListener> globalJobListeners = scheduler.getGlobalJobListeners();
+				for (final JobListener jobListener : new ArrayList<JobListener>(globalJobListeners)) {
+					if (jobListener instanceof JobGlobalListener) {
+						try {
+							scheduler.removeGlobalJobListener(jobListener);
+						} catch (final NoSuchMethodError e1) {
+							// pour Quartz 1.7, 1.8 et +,
+							// cette méthode n'existe pas avant Quartz 1.6
+							try {
+								final Class<? extends Scheduler> schedulerClass = scheduler
+										.getClass();
+								schedulerClass.getMethod("removeGlobalJobListener",
+										new Class[] { String.class }).invoke(scheduler,
+										new Object[] { jobListener.getName() });
+							} catch (final Exception e2) {
+								throw new IllegalArgumentException(e2); // NOPMD
+							}
+						}
+					}
+				}
+			}
+		} catch (final SchedulerException e) {
+			throw new IllegalStateException(e);
+		}
+	}
+
+	/** {@inheritDoc} */
+	public void jobToBeExecuted(JobExecutionContext context) {
+		// on calcule nous même le fullName du job pour être sûr que c'est le même que celui calculé
+		// dans HtmlJobInformationsReport.getCounterRequest
+		final JobDetail jobDetail = context.getJobDetail();
+		final String jobFullName = jobDetail.getGroup() + '.' + jobDetail.getName();
+		JOB_COUNTER.bindContextIncludingCpu(jobFullName);
+	}
+
+	/** {@inheritDoc} */
+	public void jobExecutionVetoed(JobExecutionContext context) {
+		JOB_COUNTER.unbindContext();
+	}
+
+	/** {@inheritDoc} */
+	public void jobWasExecuted(JobExecutionContext context, JobExecutionException jobException) {
+		// sera recalculé: final long jobRunTime = context.getJobRunTime();
+		final String stackTrace;
+		if (jobException == null) {
+			stackTrace = null;
+		} else {
+			final StringWriter stackTraceWriter = new StringWriter(200);
+			jobException.printStackTrace(new PrintWriter(stackTraceWriter));
+			stackTrace = stackTraceWriter.toString();
+		}
+		// on enregistre la requête dans les statistiques
+		JOB_COUNTER.addRequestForCurrentContext(stackTrace);
+	}
+
+	/** {@inheritDoc} */
+	public String getName() {
+		return getClass().getName();
+	}
+}
---
+++ tags/javamelody-core-1.27.0/src/test/java/net/bull/javamelody/TestMBeans.java
@@ -0,0 +1,178 @@
+/*
+ * Copyright 2008-2010 by Emeric Vernat
+ *
+ *     This file is part of Java Melody.
+ *
+ * Java Melody is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Java Melody is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Java Melody.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package net.bull.javamelody;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.management.JMException;
+import javax.management.MBeanAttributeInfo;
+import javax.management.MBeanServer;
+import javax.management.MBeanServerFactory;
+import javax.management.ObjectInstance;
+import javax.management.ObjectName;
+
+import net.bull.javamelody.TestTomcatInformations.GlobalRequestProcessor;
+import net.bull.javamelody.TestTomcatInformations.ThreadPool;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Test unitaire de la classe MBeans.
+ * @author Emeric Vernat
+ */
+public class TestMBeans {
+	private MBeans mbeans;
+	private MBeanServer mBeanServer;
+	private final List<ObjectName> mbeansList = new ArrayList<ObjectName>();
+
+	/** Before.
+	 * @throws JMException e */
+	@Before
+	public void setUp() throws JMException {
+		Utils.initialize();
+		mbeans = new MBeans();
+		mBeanServer = MBeanServerFactory.findMBeanServer(null).get(0);
+		final ObjectInstance mBean1 = mBeanServer.registerMBean(new ThreadPool(), new ObjectName(
+				"Catalina:type=ThreadPool"));
+		mbeansList.add(mBean1.getObjectName());
+		final ObjectInstance mBean2 = mBeanServer.registerMBean(new GlobalRequestProcessor(),
+				new ObjectName("Catalina:type=GlobalRequestProcessor,name=http-8080"));
+		mbeansList.add(mBean2.getObjectName());
+		final ObjectInstance mBean3 = mBeanServer.registerMBean(new GlobalRequestProcessor(),
+				new ObjectName("jonas:j2eeType=Servlet"));
+		mbeansList.add(mBean3.getObjectName());
+		final ObjectInstance mBean4 = mBeanServer.registerMBean(new GlobalRequestProcessor(),
+				new ObjectName("notjonas:type=Servlet"));
+		mbeansList.add(mBean4.getObjectName());
+		final ObjectInstance mBean5 = mBeanServer.registerMBean(new GlobalRequestProcessor(),
+				new ObjectName("jboss.deployment:type=Servlet"));
+		mbeansList.add(mBean5.getObjectName());
+	}
+
+	/** After.
+	 * @throws JMException e */
+	@After
+	public void tearDown() throws JMException {
+		for (final ObjectName registeredMBean : mbeansList) {
+			mBeanServer.unregisterMBean(registeredMBean);
+		}
+	}
+
+	/** Test.
+	 * @throws JMException e */
+	@Test
+	public void testGetThreadPools() throws JMException {
+		assertNotNull("getThreadPools", mbeans.getThreadPools());
+	}
+
+	/** Test.
+	 * @throws JMException e */
+	@Test
+	public void testGetGlobalRequestProcessors() throws JMException {
+		assertNotNull("getGlobalRequestProcessors", mbeans.getGlobalRequestProcessors());
+	}
+
+	/** Test.
+	 * @throws JMException e */
+	@Test
+	public void testGetAttribute() throws JMException {
+		assertNotNull("getAttribute", mbeans.getAttribute(mbeansList.get(0), "currentThreadsBusy"));
+	}
+
+	/** Test. */
+	@Test
+	public void testGetMapObjectNamesByDomainAndFirstProperty() {
+		assertNotNull("getMapObjectNamesByDomainAndFirstProperty",
+				mbeans.getMapObjectNamesByDomainAndFirstProperty());
+	}
+
+	/** Test.
+	 * @throws JMException e */
+	@Test
+	public void testGetMBeanInfo() throws JMException {
+		assertNotNull("getMBeanInfo", mbeans.getMBeanInfo(mbeansList.get(0)));
+	}
+
+	/** Test.
+	 * @throws JMException e
+	 * @throws IllegalAccessException e */
+	@Test
+	public void testGetAttributes() throws JMException, IllegalAccessException {
+		final MBeanAttributeInfo[] attributes = mbeans.getMBeanInfo(mbeansList.get(0))
+				.getAttributes();
+		assertNotNull("getAttributes", mbeans.getAttributes(mbeansList.get(0), attributes));
+		JdbcWrapperHelper.setFieldValue(attributes[attributes.length - 1], "name", "Password");
+		assertNotNull("getAttributes", mbeans.getAttributes(mbeansList.get(0), attributes));
+		JdbcWrapperHelper.setFieldValue(attributes[attributes.length - 1], "isRead", Boolean.FALSE);
+		assertNotNull("getAttributes", mbeans.getAttributes(mbeansList.get(0), attributes));
+	}
+
+	/** Test. */
+	@Test
+	public void testGetConvertedAttribute() {
+		final String firstMBean = mbeansList.get(0).toString();
+		final String message = "getConvertedAttributes";
+		assertNotNull(message, MBeans.getConvertedAttributes(firstMBean + ".maxThreads"));
+		assertNotNull(
+				message,
+				MBeans.getConvertedAttributes(firstMBean + ".maxThreads|" + firstMBean
+						+ ".maxThreads"));
+		assertNotNull(message, MBeans.getConvertedAttributes(firstMBean + ".intArrayAsInJRockit"));
+		assertNotNull(message,
+				MBeans.getConvertedAttributes(firstMBean + ".doubleArrayAsInJRockit"));
+		try {
+			MBeans.getConvertedAttributes("Catalina:type=instanceNotFound.maxThreads");
+		} catch (final IllegalArgumentException e) {
+			assertNotNull("e", e);
+		}
+		try {
+			MBeans.getConvertedAttributes("n'importe quoi.maxThreads");
+		} catch (final IllegalArgumentException e) {
+			assertNotNull("e", e);
+		}
+		try {
+			MBeans.getConvertedAttributes(firstMBean + ".Password");
+		} catch (final IllegalArgumentException e) {
+			assertNotNull("e", e);
+		}
+		try {
+			MBeans.getConvertedAttributes("noAttribute");
+		} catch (final IllegalArgumentException e) {
+			assertNotNull("e", e);
+		}
+	}
+
+	/** Test.
+	 * @throws JMException e */
+	@Test
+	public void testGetAttributeDescription() throws JMException {
+		assertNotNull("getAttributeDescription", mbeans.getAttributeDescription("maxThreads",
+				mbeans.getMBeanInfo(mbeansList.get(0)).getAttributes()));
+		assertNull(
+				"getAttributeDescription",
+				mbeans.getAttributeDescription("n'existe pas",
+						mbeans.getMBeanInfo(mbeansList.get(0)).getAttributes()));
+	}
+}
