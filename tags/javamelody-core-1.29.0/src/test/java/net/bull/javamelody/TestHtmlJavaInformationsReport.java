c2d8081625719e8f7cf629c52aee3a3e7076248c
evernat@free.fr <evernat@free.fr@80d463d6-a79d-11de-ba6e-69e41c8db662>
2011-05-30 20:36:34 +0000

    [maven-release-plugin]  copy for tag javamelody-core-1.29.0

---
+++ tags/javamelody-core-1.29.0/src/test/java/net/bull/javamelody/TestHtmlJavaInformationsReport.java
@@ -0,0 +1,118 @@
+/*
+ * Copyright 2008-2010 by Emeric Vernat
+ *
+ *     This file is part of Java Melody.
+ *
+ * Java Melody is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Java Melody is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Java Melody.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package net.bull.javamelody; // NOPMD
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.management.JMException;
+import javax.management.MBeanServer;
+import javax.management.MBeanServerFactory;
+import javax.management.ObjectName;
+
+import net.bull.javamelody.TestTomcatInformations.GlobalRequestProcessor;
+import net.bull.javamelody.TestTomcatInformations.ThreadPool;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Test unitaire de la classe HtmlJavaInformationsReport.
+ * @author Emeric Vernat
+ */
+public class TestHtmlJavaInformationsReport {
+	private StringWriter writer;
+
+	/** Initialisation. */
+	@Before
+	public void setUp() {
+		Utils.initialize();
+		// pour testTomcatInformations (si la classe TomcatInformations est déjà chargée,
+		// c'est trop tard, et c'est pourquoi cela doit être défini au lancement de junit)
+		System.setProperty("catalina.home", "unknown");
+		writer = new StringWriter();
+	}
+
+	/** Test.
+	 * @throws IOException e
+	 * @throws JMException e */
+	@Test
+	public void testTomcatInformations() throws IOException, JMException {
+		final List<MBeanServer> mBeanServerList = MBeanServerFactory.findMBeanServer(null);
+		final MBeanServer mBeanServer;
+		if (mBeanServerList.isEmpty()) {
+			mBeanServer = MBeanServerFactory.createMBeanServer();
+		} else {
+			mBeanServer = mBeanServerList.get(0);
+		}
+		final List<ObjectName> mBeans = new ArrayList<ObjectName>();
+		try {
+			mBeans.add(mBeanServer.registerMBean(new ThreadPool(),
+					new ObjectName("Catalina:type=ThreadPool,name=jk-8009")).getObjectName());
+			mBeans.add(mBeanServer.registerMBean(new GlobalRequestProcessor(),
+					new ObjectName("Catalina:type=GlobalRequestProcessor,name=jk-8009"))
+					.getObjectName());
+			TomcatInformations.initMBeans();
+			final List<JavaInformations> myJavaInformationsList = Arrays
+					.asList(new JavaInformations(null, true));
+			final HtmlJavaInformationsReport htmlReport = new HtmlJavaInformationsReport(
+					myJavaInformationsList, writer);
+			htmlReport.toHtml();
+			assertNotEmptyAndClear(writer);
+
+			mBeans.add(mBeanServer.registerMBean(new ThreadPool(),
+					new ObjectName("Catalina:type=ThreadPool,name=jk-8010")).getObjectName());
+			final GlobalRequestProcessor jk8010 = new GlobalRequestProcessor();
+			jk8010.setrequestCount(0);
+			mBeans.add(mBeanServer.registerMBean(jk8010,
+					new ObjectName("Catalina:type=GlobalRequestProcessor,name=jk-8010"))
+					.getObjectName());
+			TomcatInformations.initMBeans();
+			final List<JavaInformations> myJavaInformationsList2 = Arrays
+					.asList(new JavaInformations(null, true));
+			final HtmlJavaInformationsReport htmlReport2 = new HtmlJavaInformationsReport(
+					myJavaInformationsList2, writer);
+			htmlReport2.toHtml();
+			assertNotEmptyAndClear(writer);
+
+			jk8010.setrequestCount(1000);
+			final List<JavaInformations> myJavaInformationsList3 = Arrays
+					.asList(new JavaInformations(null, true));
+			final HtmlJavaInformationsReport htmlReport3 = new HtmlJavaInformationsReport(
+					myJavaInformationsList3, writer);
+			htmlReport3.toHtml();
+			assertNotEmptyAndClear(writer);
+		} finally {
+			for (final ObjectName registeredMBean : mBeans) {
+				mBeanServer.unregisterMBean(registeredMBean);
+			}
+			TomcatInformations.initMBeans();
+		}
+	}
+
+	private static void assertNotEmptyAndClear(final StringWriter writer) {
+		assertTrue("rapport vide", writer.getBuffer().length() > 0);
+		writer.getBuffer().setLength(0);
+	}
+}
---
+++ tags/javamelody-core-1.29.0/src/test/test-webapp/jsp/directSql.jsp
@@ -0,0 +1,30 @@
+<%@ page session="false" %>
+<%	
+	final java.util.Properties properties = new java.util.Properties();
+	final String query = "select sleep(.1)";
+	final String url = "jdbc:mysql://127.0.0.1:3306/test?socketTimeout=5000&amp;connectTimeout=5000";
+	properties.put("user", "root");
+    properties.put("password", "root");
+    properties.put("driver", "com.mysql.jdbc.Driver");
+    
+    //final String query = "select 1 from dual";
+    //final String url = "jdbc:oracle:thin:@127.0.0.1:1521:";
+    //properties.put("user", "test");
+    //properties.put("password", "test");
+    //properties.put("driver", "oracle.jdbc.OracleDriver");
+    
+    Class.forName("net.bull.javamelody.JdbcDriver");
+	final java.sql.Connection connection = java.sql.DriverManager.getConnection(url, properties);
+	connection.setAutoCommit(false);
+	try {
+		connection.createStatement().executeQuery(query);
+	} finally {
+		connection.rollback();
+		connection.close();
+	}
+%>
+
+direct sql done
+
+<br/>
+<a href="../index.jsp">back</a>
