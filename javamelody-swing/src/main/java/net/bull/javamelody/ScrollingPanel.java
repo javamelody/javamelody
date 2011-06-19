d9a2b5bd50a13e333c3a3c97983e6bea11ef78a2
evernat <evernat@free.fr>
2011-06-19 21:42:13 +0000

    refactoring

---
+++ javamelody-swing/src/main/java/net/bull/javamelody/ScrollingPanel.java
@@ -0,0 +1,130 @@
+/*
+ * Copyright 2008-2010 by Emeric Vernat
+ *
+ *     This file is part of Java Melody.
+ *
+ * Java Melody is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Java Melody is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Java Melody.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package net.bull.javamelody;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+/**
+ * Panel principal.
+ * @author Emeric Vernat
+ */
+class ScrollingPanel extends JPanel {
+	private static final long serialVersionUID = 1L;
+
+	@SuppressWarnings("all")
+	private final Collector collector;
+	@SuppressWarnings("all")
+	private final List<JavaInformations> javaInformationsList;
+	// TODO range selon sélection (jour par défaut)
+	private final Range range = Period.TOUT.getRange();
+
+	ScrollingPanel(Collector collector, List<JavaInformations> javaInformationsList)
+			throws IOException {
+		super();
+		this.collector = collector;
+		this.javaInformationsList = javaInformationsList;
+
+		setBorder(BorderFactory.createEmptyBorder(3, 3, 3, 3));
+		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		setBackground(new Color(230, 230, 230));
+		add(new ChartsPanel());
+
+		addCounters();
+
+		addSystemInformations();
+
+		addThreadInformations();
+
+		for (final Component component : getComponents()) {
+			((JComponent) component).setAlignmentX(Component.LEFT_ALIGNMENT);
+		}
+	}
+
+	private void addCounters() throws IOException {
+		for (final Counter counter : collector.getRangeCountersToBeDisplayed(range)) {
+			final String counterLabel = I18N.getString(counter.getName() + "Label");
+			addParagraphTitle(I18N.getFormattedString("Statistiques_compteur", counterLabel)
+					+ " - " + range.getLabel(), counter.getIconName());
+			final StatisticsPanel statisticsPanel = new StatisticsPanel(counter, range);
+			statisticsPanel.showGlobalRequests();
+			add(statisticsPanel);
+		}
+	}
+
+	private void addSystemInformations() {
+		addParagraphTitle(I18N.getString("Informations_systemes"), "systeminfo.png");
+		final List<JavaInformations> list = javaInformationsList;
+		add(new SystemInformationsButtonsPanel(list));
+
+		final List<JavaInformationsPanel> javaInformationsPanelList = new ArrayList<JavaInformationsPanel>(
+				list.size());
+		final JPanel westJavaInformationsPanel = new JPanel(new FlowLayout(FlowLayout.LEFT, 5, 0));
+		westJavaInformationsPanel.setOpaque(false);
+		for (final JavaInformations javaInformations : list) {
+			final JavaInformationsPanel javaInformationsPanel = new JavaInformationsPanel(
+					javaInformations);
+			javaInformationsPanel.showSummary();
+			javaInformationsPanelList.add(javaInformationsPanel);
+			westJavaInformationsPanel.add(javaInformationsPanel);
+		}
+		final JButton javaInformationsDetailsButton = new JButton(I18N.getString("Details"));
+		javaInformationsDetailsButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				final boolean repeatHost = list.size() > 1;
+				for (final JavaInformationsPanel javaInformationsPanel : javaInformationsPanelList) {
+					javaInformationsPanel.showDetails(repeatHost);
+				}
+			}
+		});
+		westJavaInformationsPanel.add(javaInformationsDetailsButton);
+		add(westJavaInformationsPanel);
+		add(new JLabel(" "));
+	}
+
+	private void addThreadInformations() {
+		addParagraphTitle(I18N.getString("Threads"), "threads.png");
+		for (final JavaInformations javaInformations : javaInformationsList) {
+			javaInformations.getThreadInformationsList();
+		}
+	}
+
+	private void addParagraphTitle(String title, String iconName) {
+		final JLabel label = new JLabel(title);
+		label.setIcon(ImageIconCache.getScaledImageIcon(iconName, 24, 24));
+		label.setFont(label.getFont().deriveFont(Font.BOLD, label.getFont().getSize() + 4));
+		add(label);
+		add(new JLabel(" ")); // séparateur
+	}
+}
