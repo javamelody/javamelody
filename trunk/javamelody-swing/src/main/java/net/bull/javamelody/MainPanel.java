ddf186c473ce783aef2a7548d24d5deca417a4f3
evernat@free.fr <evernat@free.fr@80d463d6-a79d-11de-ba6e-69e41c8db662>
2011-06-19 21:42:13 +0000

    refactoring

--- trunk/javamelody-swing/src/main/java/net/bull/javamelody/MainPanel.java
+++ trunk/javamelody-swing/src/main/java/net/bull/javamelody/MainPanel.java
@@ -19,17 +19,10 @@
 package net.bull.javamelody;
 
 import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Component;
-import java.awt.Font;
 import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 
-import javax.swing.BorderFactory;
-import javax.swing.BoxLayout;
-import javax.swing.JComponent;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.ScrollPaneConstants;
@@ -41,42 +34,12 @@ import javax.swing.ScrollPaneConstants;
 class MainPanel extends JPanel {
 	private static final long serialVersionUID = 1L;
 
-	@SuppressWarnings("all")
-	private final Collector collector;
-	// TODO range selon sélection (jour par défaut)
-	private final Range range = Period.TOUT.getRange();
-	private final JPanel scrollPanel = new JPanel();
-
 	MainPanel(Collector collector, List<JavaInformations> javaInformationsList, URL onlineHelpUrl)
 			throws IOException {
 		super();
-		this.collector = collector;
-
 		setLayout(new BorderLayout());
-
-		scrollPanel.setBorder(BorderFactory.createEmptyBorder(3, 3, 3, 3));
-		scrollPanel.setLayout(new BoxLayout(scrollPanel, BoxLayout.Y_AXIS));
-		scrollPanel.add(new ChartsPanel());
-		scrollPanel.setBackground(new Color(230, 230, 230));
-		for (final Counter counter : collector.getRangeCountersToBeDisplayed(range)) {
-			final String counterLabel = I18N.getString(counter.getName() + "Label");
-			addParagraphTitle(I18N.getFormattedString("Statistiques_compteur", counterLabel)
-					+ " - " + range.getLabel(), counter.getIconName());
-			final StatisticsPanel statisticsPanel = new StatisticsPanel(counter, range);
-			statisticsPanel.showGlobalRequests();
-			scrollPanel.add(statisticsPanel);
-		}
-		for (final JavaInformations javaInformations : javaInformationsList) {
-			addParagraphTitle(I18N.getString("Informations_systemes"), "systeminfo.png");
-			final JPanel westPanel = new JPanel(new BorderLayout());
-			westPanel.setOpaque(false);
-			westPanel.add(new JavaInformationsPanel(javaInformations), BorderLayout.WEST);
-			scrollPanel.add(westPanel);
-		}
-		for (final Component component : scrollPanel.getComponents()) {
-			((JComponent) component).setAlignmentX(Component.LEFT_ALIGNMENT);
-		}
-		final JScrollPane scrollPane = new JScrollPane(scrollPanel);
+		final ScrollingPanel scrollingPanel = new ScrollingPanel(collector, javaInformationsList);
+		final JScrollPane scrollPane = new JScrollPane(scrollingPanel);
 		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
 		final JPanel southPanel = new JPanel(new BorderLayout());
 		southPanel.setOpaque(false);
@@ -85,12 +48,5 @@ class MainPanel extends JPanel {
 
 		add(new MainButtonsPanel(onlineHelpUrl), BorderLayout.NORTH);
 		add(southPanel, BorderLayout.CENTER);
-	}
-
-	private void addParagraphTitle(String title, String iconName) {
-		final JLabel label = new JLabel(title);
-		label.setIcon(ImageIconCache.getScaledImageIcon(iconName, 24, 24));
-		label.setFont(label.getFont().deriveFont(Font.BOLD, label.getFont().getSize() + 4));
-		scrollPanel.add(label);
 	}
 }
