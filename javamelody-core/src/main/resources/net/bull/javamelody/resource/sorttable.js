7012ca60dea21abfa61b32de6d7e10c6a219ef62
evernat <evernat@free.fr>
2011-06-24 23:25:38 +0000

    fix pour issue 117: Sorting of numbers is based on String comparison in German

--- javamelody-core/src/main/resources/net/bull/javamelody/resource/sorttable.js
+++ javamelody-core/src/main/resources/net/bull/javamelody/resource/sorttable.js
@@ -262,9 +262,12 @@ sorttable = {
      each sort function takes two parameters, a and b
      you are comparing a[0] and b[0] */
   sort_numeric: function(a,b) {
-    aa = parseFloat(a[0].replace(/[^0-9.-]/g,''));
+    //aa = parseFloat(a[0].replace(/[^0-9.-]/g,''));
+	//issue 117: this is ok for this app because of number of decimals
+    aa = parseFloat(a[0].replace(/[^0-9-]/g,''));
     if (isNaN(aa)) aa = 0;
-    bb = parseFloat(b[0].replace(/[^0-9.-]/g,'')); 
+    //issue 117: bb = parseFloat(b[0].replace(/[^0-9.-]/g,'')); 
+    bb = parseFloat(b[0].replace(/[^0-9-]/g,'')); 
     if (isNaN(bb)) bb = 0;
     return aa-bb;
   },
