0a6c85e556cf332a91ece7a8c17a756b0ef45a48
evernat <evernat@free.fr>
2011-07-18 20:46:56 +0000

    compléments + layout

--- javamelody-swing/src/main/java/net/bull/javamelody/JavaInformationsPanel.java
+++ javamelody-swing/src/main/java/net/bull/javamelody/JavaInformationsPanel.java
@@ -19,11 +19,14 @@
 package net.bull.javamelody;
 
 import java.awt.BorderLayout;
-import java.awt.GridLayout;
 import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SpringLayout;
+import javax.swing.SwingConstants;
 
 import com.lowagie.text.Font;
 
@@ -42,12 +45,10 @@ class JavaInformationsPanel extends JPanel {
 	private JavaInformationsPanel detailsPanel;
 
 	JavaInformationsPanel(JavaInformations javaInformations) {
-		super();
+		super(new BorderLayout());
 		this.javaInformations = javaInformations;
 		setOpaque(false);
-		setLayout(new BorderLayout());
-		// TODO mettre des hauteurs variables selon les labels
-		gridPanel = new JPanel(new GridLayout(-1, 2, 10, 0));
+		gridPanel = new JPanel(new SpringLayout());
 		gridPanel.setOpaque(false);
 		add(gridPanel, BorderLayout.NORTH);
 	}
@@ -71,7 +72,7 @@ class JavaInformationsPanel extends JPanel {
 			// 			writeGraph("httpSessions", integerFormat.format(javaInformations.getSessionCount()));
 			addValue(integerFormat.format(javaInformations.getSessionCount()));
 		}
-		addLabel("<html>" + I18N.getString("nb_threads_actifs") + "<br>("
+		addLabel(I18N.getString("nb_threads_actifs") + "\n("
 				+ I18N.getString("Requetes_http_en_cours") + ')');
 		//		writeGraph("activeThreads", integerFormat.format(javaInformations.getActiveThreadCount()));
 		addValue(integerFormat.format(javaInformations.getActiveThreadCount()));
@@ -81,8 +82,8 @@ class JavaInformationsPanel extends JPanel {
 			addValue(integerFormat.format(javaInformations.getActiveConnectionCount()));
 			final int usedConnectionCount = javaInformations.getUsedConnectionCount();
 			final int maxConnectionCount = javaInformations.getMaxConnectionCount();
-			addLabel("<html>" + I18N.getString("nb_connexions_utilisees") + "<br>("
-					+ I18N.getString("ouvertes") + ')');
+			addLabel(I18N.getString("nb_connexions_utilisees") + "\n(" + I18N.getString("ouvertes")
+					+ ')');
 			//			writeGraph("usedConnections", integerFormat.format(usedConnectionCount));
 			if (maxConnectionCount > 0) {
 				addValue(integerFormat.format(usedConnectionCount));
@@ -97,6 +98,7 @@ class JavaInformationsPanel extends JPanel {
 			//			writeGraph("systemLoad", decimalFormat.format(javaInformations.getSystemLoadAverage()));
 			addValue(decimalFormat.format(javaInformations.getSystemLoadAverage()));
 		}
+		makeGrid();
 	}
 
 	void showDetails(boolean repeatHost) {
@@ -138,32 +140,31 @@ class JavaInformationsPanel extends JPanel {
 		//		}
 		addLabel(I18N.getString("PID"));
 		addValue(javaInformations.getPID());
-		//		final long unixOpenFileDescriptorCount = javaInformations.getUnixOpenFileDescriptorCount();
-		//		if (unixOpenFileDescriptorCount >= 0) {
-		//			final long unixMaxFileDescriptorCount = javaInformations
-		//					.getUnixMaxFileDescriptorCount();
-		//			write("<tr><td>#nb_fichiers#</td><td>");
-		//			writeGraph("fileDescriptors", integerFormat.format(unixOpenFileDescriptorCount));
-		//			writeln(" / " + integerFormat.format(unixMaxFileDescriptorCount) + "&nbsp;&nbsp;&nbsp;");
-		//			writeln(toBar(javaInformations.getUnixOpenFileDescriptorPercentage()));
-		//			writeln(columnEnd);
-		//		}
-		//		writeServerInfoAndContextPath(javaInformations);
+		final long unixOpenFileDescriptorCount = javaInformations.getUnixOpenFileDescriptorCount();
+		if (unixOpenFileDescriptorCount >= 0) {
+			final long unixMaxFileDescriptorCount = javaInformations
+					.getUnixMaxFileDescriptorCount();
+			addLabel(I18N.getString("nb_fichiers"));
+			addValue(integerFormat.format(unixOpenFileDescriptorCount) + " / "
+					+ integerFormat.format(unixMaxFileDescriptorCount));
+			// writeGraph("fileDescriptors", integerFormat.format(unixOpenFileDescriptorCount));
+			// writeln(toBar(javaInformations.getUnixOpenFileDescriptorPercentage()));
+		}
+		writeServerInfoAndContextPath();
 		addLabel(I18N.getString("Demarrage"));
 		addValue(I18N.createDateAndTimeFormat().format(javaInformations.getStartDate()));
-		// TODO
-		//		addLabel(I18N.getString("Arguments_JVM"));
-		//		addValue("<html>" + javaInformations.getJvmArguments().replace("\n", "<br/>"));
+		addLabel(I18N.getString("Arguments_JVM"));
+		addValue(javaInformations.getJvmArguments());
 
 		if (javaInformations.getSessionCount() >= 0) {
 			addLabel(I18N.getString("httpSessionsMeanAge"));
 			// writeGraph("httpSessionsMeanAge", integerFormat.format(javaInformations.getSessionMeanAgeInMinutes()));
 			addValue(integerFormat.format(javaInformations.getSessionMeanAgeInMinutes()));
 		}
-		//
-		//		writeTomcatInformations(javaInformations.getTomcatInformationsList());
-		//
-		//		writeMemoryInformations(javaInformations.getMemoryInformations());
+
+		writeTomcatInformations(javaInformations.getTomcatInformationsList());
+
+		writeMemoryInformations(javaInformations.getMemoryInformations());
 
 		if (javaInformations.getFreeDiskSpaceInTemp() >= 0) {
 			// on considère que l'espace libre sur le disque dur est celui sur la partition du répertoire temporaire
@@ -172,20 +173,149 @@ class JavaInformationsPanel extends JPanel {
 					+ ' ' + I18N.getString("Mo"));
 		}
 
-		//		writeDatabaseVersionAndDataSourceDetails(javaInformations);
-		//
-		//		if (javaInformations.isDependenciesEnabled()) {
-		//			writeln("<tr><td valign='top'>#Dependencies#: </td><td>");
-		//			writeDependencies(javaInformations);
-		//			writeln(columnEnd);
-		//		}
+		writeDatabaseVersionAndDataSourceDetails();
+
+		if (javaInformations.isDependenciesEnabled()) {
+			addLabel(I18N.getString("Dependencies"));
+			writeDependencies();
+		}
+		makeGrid();
+	}
+
+	private void writeServerInfoAndContextPath() {
+		final String serverInfo = javaInformations.getServerInfo();
+		if (serverInfo != null) {
+			addLabel(I18N.getString("Serveur"));
+			final String applicationServerIconName = HtmlJavaInformationsReport
+					.getApplicationServerIconName(serverInfo);
+			final JLabel serverInfoLabel = new JLabel(serverInfo);
+			if (applicationServerIconName != null) {
+				serverInfoLabel.setIcon(ImageIconCache.getImageIcon("servers/"
+						+ applicationServerIconName));
+			}
+			gridPanel.add(serverInfoLabel);
+			addLabel(I18N.getString("Contexte_webapp"));
+			addValue(javaInformations.getContextPath());
+		}
+	}
+
+	private void writeDatabaseVersionAndDataSourceDetails() {
+		if (!noDatabase && javaInformations.getDataBaseVersion() != null) {
+			addLabel(I18N.getString("Base_de_donnees"));
+			addValue(javaInformations.getDataBaseVersion());
+		}
+		if (javaInformations.getDataSourceDetails() != null) {
+			addLabel(I18N.getString("DataSource_jdbc"));
+			addValue(javaInformations.getDataSourceDetails());
+			//					+ "<a href='http://commons.apache.org/dbcp/apidocs/org/apache/commons/dbcp/BasicDataSource.html'"
+			//					+ " class='noPrint' target='_blank'>DataSource reference</a>");
+		}
+	}
+
+	private void writeTomcatInformations(List<TomcatInformations> tomcatInformationsList) {
+		final List<TomcatInformations> list = new ArrayList<TomcatInformations>();
+		for (final TomcatInformations tomcatInformations : tomcatInformationsList) {
+			if (tomcatInformations.getRequestCount() > 0) {
+				list.add(tomcatInformations);
+			}
+		}
+		//		final boolean onlyOne = list.size() == 1;
+		final String equals = " = ";
+		for (final TomcatInformations tomcatInformations : list) {
+			addLabel("Tomcat " + I18N.htmlEncode(tomcatInformations.getName(), false));
+			// rq: on n'affiche pas pour l'instant getCurrentThreadCount
+			final int currentThreadsBusy = tomcatInformations.getCurrentThreadsBusy();
+			addValue(I18N.getString("busyThreads") + equals
+					+ integerFormat.format(currentThreadsBusy) + " /  "
+					+ integerFormat.format(tomcatInformations.getMaxThreads()) + '\n'
+					+ I18N.getString("bytesReceived") + equals
+					+ integerFormat.format(tomcatInformations.getBytesReceived()) + '\n'
+					+ I18N.getString("bytesSent") + equals
+					+ integerFormat.format(tomcatInformations.getBytesSent()) + '\n'
+					+ I18N.getString("requestCount") + equals
+					+ integerFormat.format(tomcatInformations.getRequestCount()) + '\n'
+					+ I18N.getString("errorCount") + equals
+					+ integerFormat.format(tomcatInformations.getErrorCount()) + '\n'
+					+ I18N.getString("processingTime") + equals
+					+ integerFormat.format(tomcatInformations.getProcessingTime()) + '\n'
+					+ I18N.getString("maxProcessingTime") + equals
+					+ integerFormat.format(tomcatInformations.getMaxTime()));
+			//			if (onlyOne) {
+			//				writeGraph("tomcatBusyThreads", integerFormat.format(currentThreadsBusy));
+			//          }
+			//			writeln(toBar(100d * currentThreadsBusy / tomcatInformations.getMaxThreads()));
+			//			if (onlyOne) {
+			//				writeGraph("tomcatBytesReceived",
+			//						integerFormat.format(tomcatInformations.getBytesReceived()));
+			//			}
+			//			if (onlyOne) {
+			//				writeGraph("tomcatBytesSent",
+			//						integerFormat.format(tomcatInformations.getBytesSent()));
+			//			}
+		}
+	}
+
+	private void writeMemoryInformations(MemoryInformations memoryInformations) {
+		addLabel(I18N.getString("Gestion_memoire"));
+		addValue(memoryInformations.getMemoryDetails().replace(" Mo", ' ' + I18N.getString("Mo")));
+
+		final long usedPermGen = memoryInformations.getUsedPermGen();
+		if (usedPermGen > 0) {
+			// perm gen est à 0 sous jrockit
+			final long maxPermGen = memoryInformations.getMaxPermGen();
+			addLabel(I18N.getString("Memoire_Perm_Gen"));
+			final String permGen = integerFormat.format(usedPermGen / 1024 / 1024) + ' '
+					+ I18N.getString("Mo");
+			if (maxPermGen > 0) {
+				addValue(permGen + " / " + integerFormat.format(maxPermGen / 1024 / 1024) + ' '
+						+ I18N.getString("Mo"));
+				// writeln(toBar(memoryInformations.getUsedPermGenPercentage()));
+			} else {
+				addValue(permGen);
+			}
+		}
+	}
+
+	private void writeDependencies() {
+		final int nbDependencies = javaInformations.getDependenciesList().size();
+		addValue(I18N.getFormattedString("nb_dependencies", nbDependencies));
+		if (nbDependencies > 0) {
+			//			writeln(" ; &nbsp;&nbsp;&nbsp;");
+			//			writeShowHideLink("detailsDependencies" + uniqueByPageSequence, "#Details#");
+			//			if (javaInformations.doesPomXmlExists() && Parameters.isSystemActionsEnabled()) {
+			//				writeln("&nbsp;&nbsp;&nbsp;<a href='?part=pom.xml' class='noPrint'>");
+			//				writeln("<img src='?resource=xml.png' width='14' height='14' alt=\"#pom.xml#\"/> #pom.xml#</a>");
+			//			}
+			//			writeln("<br/>");
+			//			writeln("<div id='detailsDependencies" + uniqueByPageSequence
+			//					+ "' style='display: none;'><div>");
+			//			writeln(replaceEolWithBr(javaInformations.getDependencies()));
+			//			writeln("</div></div>");
+		}
+	}
+
+	private void makeGrid() {
+		SpringUtilities.makeCompactGrid(gridPanel, gridPanel.getComponentCount() / 2, 2, 0, 0, 10,
+				5);
 	}
 
 	private void addLabel(String text) {
-		gridPanel.add(new JLabel(text + ": "));
+		String tmp = text;
+		if (tmp.indexOf('\n') != -1) {
+			// JLabel accepte la syntaxe html
+			tmp = "<html>" + tmp.replace("\n", "<br/>");
+		}
+		final JLabel label = new JLabel(tmp + ": ");
+		label.setVerticalAlignment(SwingConstants.TOP);
+		gridPanel.add(label);
 	}
 
 	private void addValue(String value) {
-		gridPanel.add(new JLabel(value));
+		String tmp = value;
+		if (tmp.indexOf('\n') != -1) {
+			// JLabel accepte la syntaxe html
+			tmp = "<html>" + tmp.replace("\n", "<br/>");
+		}
+		gridPanel.add(new JLabel(tmp));
 	}
 }
