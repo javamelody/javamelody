843778078e5abb91b707ed0da8d68d8df48aefdc
evernat@free.fr <evernat@free.fr@80d463d6-a79d-11de-ba6e-69e41c8db662>
2011-08-01 21:10:23 +0000

    relecture

--- trunk/javamelody-swing/src/main/java/net/bull/javamelody/table/MDefaultTableCellRenderer.java
+++ trunk/javamelody-swing/src/main/java/net/bull/javamelody/table/MDefaultTableCellRenderer.java
@@ -36,9 +36,9 @@ public class MDefaultTableCellRenderer extends DefaultTableCellRenderer {
 	 * Ajustement de la hauteur de cette ligne en fonction de la taille du renderer (ex: la taille d'une icône ou d'un label html).
 	 *
 	 * @param table
-	 *           javax.swing.JTable
+	 *           JTable
 	 * @param component
-	 *           java.awt.Component
+	 *           Component
 	 * @param row
 	 *           int
 	 */
@@ -46,7 +46,7 @@ public class MDefaultTableCellRenderer extends DefaultTableCellRenderer {
 		// Ajustement de la hauteur de cette ligne en fonction de la taille du renderer
 		final int cellHeight = table.getRowHeight(row);
 		final int rendererHeight = component.getPreferredSize().height;
-		if (cellHeight < rendererHeight - 2) { // dans le cas normal, cellHeight est � 16 et rendererHeight est � 18
+		if (cellHeight < rendererHeight - 2) { // dans le cas normal, cellHeight est à 16 et rendererHeight est à 18
 			table.setRowHeight(row, rendererHeight);
 		}
 	}
