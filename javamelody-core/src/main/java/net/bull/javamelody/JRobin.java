83ad93c17f2aca92dd23ecb7eb28673f763c049c
evernat <evernat@free.fr>
2011-09-25 14:44:30 +0000

    fix pour issue 141 : exception while collecting data java.io.IOException: Read failed, file xyz not mapped for I/O

--- javamelody-core/src/main/java/net/bull/javamelody/JRobin.java
+++ javamelody-core/src/main/java/net/bull/javamelody/JRobin.java
@@ -138,8 +138,10 @@ final class JRobin {
 			throw new IOException("JavaMelody directory can't be created: "
 					+ rrdDirectory.getPath());
 		}
-		if (!rrdFile.exists()) {
-			// create RRD file since it does not exist
+		// cf issue 41: rrdFile could have been created with length 0 if out of disk space
+		// (fix IOException: Read failed, file xxx.rrd not mapped for I/O)
+		if (!rrdFile.exists() || rrdFile.length() == 0) {
+			// create RRD file since it does not exist (or is empty)
 			final RrdDef rrdDef = new RrdDef(rrdFileName, step);
 			// "startTime" décalé de "step" pour éviter que addValue appelée juste
 			// après ne lance l'exception suivante la première fois
