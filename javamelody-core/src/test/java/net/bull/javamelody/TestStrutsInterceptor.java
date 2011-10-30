0c3ecf487fe8374c1270f081fe5da20cf4b18f0c
evernat <evernat@free.fr>
2011-10-30 19:06:56 +0000

    correction test unitaire suite à évolution de SrutsInterceptor

--- javamelody-core/src/test/java/net/bull/javamelody/TestStrutsInterceptor.java
+++ javamelody-core/src/test/java/net/bull/javamelody/TestStrutsInterceptor.java
@@ -25,13 +25,11 @@ import static org.easymock.EasyMock.verify;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertSame;
 
-import java.util.HashMap;
-
 import org.junit.Before;
 import org.junit.Test;
 
-import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionInvocation;
+import com.opensymphony.xwork2.mock.MockActionProxy;
 
 /**
  * Test unitaire de la classe StrutsInterceptor.
@@ -58,9 +56,12 @@ public class TestStrutsInterceptor {
 		strutsCounter.clear();
 		final StrutsInterceptor strutsInterceptor = new StrutsInterceptor();
 		final ActionInvocation invocation = createNiceMock(ActionInvocation.class);
-		final ActionContext context = new ActionContext(new HashMap<Object, Object>());
-		context.setName("test.action");
-		expect(invocation.getInvocationContext()).andReturn(context).anyTimes();
+		final MockActionProxy proxy = new MockActionProxy();
+		proxy.setInvocation(invocation);
+		proxy.setActionName("test.action");
+		proxy.setMethod(null);
+		proxy.setNamespace("testnamespace");
+		expect(invocation.getProxy()).andReturn(proxy).anyTimes();
 
 		replay(invocation);
 		strutsCounter.setDisplayed(false);
@@ -74,18 +75,34 @@ public class TestStrutsInterceptor {
 		verify(invocation);
 
 		final ActionInvocation invocation2 = createNiceMock(ActionInvocation.class);
-		final ActionContext context2 = new ActionContext(new HashMap<Object, Object>());
-		context2.setName("test2.action");
-		expect(invocation2.getInvocationContext()).andReturn(context2).anyTimes();
-		expect(invocation2.invoke()).andThrow(new UnknownError("test d'erreur")).anyTimes();
+		final MockActionProxy proxy2 = new MockActionProxy();
+		proxy2.setInvocation(invocation2);
+		proxy2.setActionName("test2.action");
+		proxy2.setMethod("execute");
+		proxy2.setNamespace("testnamespace");
+		expect(invocation2.getProxy()).andReturn(proxy2).anyTimes();
 
 		replay(invocation2);
+		strutsInterceptor.intercept(invocation2);
+		assertSame(requestsCount, 2, strutsCounter.getRequestsCount());
+		verify(invocation2);
+
+		final ActionInvocation invocation3 = createNiceMock(ActionInvocation.class);
+		final MockActionProxy proxy3 = new MockActionProxy();
+		proxy3.setInvocation(invocation3);
+		proxy3.setActionName("test3.action");
+		proxy3.setMethod("testmethod");
+		proxy3.setNamespace("testnamespace");
+		expect(invocation3.getProxy()).andReturn(proxy3).anyTimes();
+		expect(invocation3.invoke()).andThrow(new UnknownError("test d'erreur")).anyTimes();
+
+		replay(invocation3);
 		try {
-			strutsInterceptor.intercept(invocation2);
+			strutsInterceptor.intercept(invocation3);
 		} catch (final UnknownError e) {
 			assertNotNull("ok", e);
 		}
-		assertSame(requestsCount, 2, strutsCounter.getRequestsCount());
-		verify(invocation2);
+		assertSame(requestsCount, 3, strutsCounter.getRequestsCount());
+		verify(invocation3);
 	}
 }
