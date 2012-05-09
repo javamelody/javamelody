9456fb525d1eaab778c64ec7acfb4c0bb05d5739
evernat <evernat@free.fr>
2012-05-09 22:53:23 +0000

    clean-up, suppresswarnings, etc (changing the minimum)

--- javamelody-core/src/main/java/net/bull/javamelody/Base64Coder.java
+++ javamelody-core/src/main/java/net/bull/javamelody/Base64Coder.java
@@ -26,33 +26,43 @@ package net.bull.javamelody;
  * Project home page: <a href="http://www.source-code.biz/base64coder/java/">www.source-code.biz/base64coder/java</a><br>
  * Author: Christian d'Heureuse, Inventec Informatik AG, Zurich, Switzerland<br>
  * Multi-licensed: EPL / LGPL / GPL / AL / BSD / MIT.
+ * Changed on format and java syntax by Emeric Vernat
  */
-public class Base64Coder {
+public final class Base64Coder { // NOPMD
 
 	// The line separator string of the operating system.
-	private static final String systemLineSeparator = System.getProperty("line.separator");
+	private static final String SYSTEM_LINE_SEPARATOR = System.getProperty("line.separator");
 
 	// Mapping table from 6-bit nibbles to Base64 characters.
-	private static final char[] map1 = new char[64];
+	private static final char[] MAP1 = new char[64];
 	static {
 		int i = 0;
-		for (char c = 'A'; c <= 'Z'; c++)
-			map1[i++] = c;
-		for (char c = 'a'; c <= 'z'; c++)
-			map1[i++] = c;
-		for (char c = '0'; c <= '9'; c++)
-			map1[i++] = c;
-		map1[i++] = '+';
-		map1[i++] = '/';
+		for (char c = 'A'; c <= 'Z'; c++) {
+			MAP1[i++] = c;
+		}
+		for (char c = 'a'; c <= 'z'; c++) {
+			MAP1[i++] = c;
+		}
+		for (char c = '0'; c <= '9'; c++) {
+			MAP1[i++] = c;
+		}
+		MAP1[i++] = '+';
+		MAP1[i++] = '/';
 	}
 
 	// Mapping table from Base64 characters to 6-bit nibbles.
-	private static final byte[] map2 = new byte[128];
+	private static final byte[] MAP2 = new byte[128];
 	static {
-		for (int i = 0; i < map2.length; i++)
-			map2[i] = -1;
-		for (int i = 0; i < 64; i++)
-			map2[map1[i]] = (byte) i;
+		for (int i = 0; i < MAP2.length; i++) {
+			MAP2[i] = -1;
+		}
+		for (int i = 0; i < 64; i++) {
+			MAP2[MAP1[i]] = (byte) i;
+		}
+	}
+
+	// Dummy constructor.
+	private Base64Coder() {
 	}
 
 	/**
@@ -72,7 +82,7 @@ public class Base64Coder {
 	* @return    A String containing the Base64 encoded data, broken into lines.
 	*/
 	public static String encodeLines(byte[] in) {
-		return encodeLines(in, 0, in.length, 76, systemLineSeparator);
+		return encodeLines(in, 0, in.length, 76, SYSTEM_LINE_SEPARATOR);
 	}
 
 	/**
@@ -86,15 +96,16 @@ public class Base64Coder {
 	*/
 	public static String encodeLines(byte[] in, int iOff, int iLen, int lineLen,
 			String lineSeparator) {
-		int blockLen = (lineLen * 3) / 4;
-		if (blockLen <= 0)
+		final int blockLen = lineLen * 3 / 4;
+		if (blockLen <= 0) {
 			throw new IllegalArgumentException();
-		int lines = (iLen + blockLen - 1) / blockLen;
-		int bufLen = ((iLen + 2) / 3) * 4 + lines * lineSeparator.length();
-		StringBuilder buf = new StringBuilder(bufLen);
+		}
+		final int lines = (iLen + blockLen - 1) / blockLen;
+		final int bufLen = (iLen + 2) / 3 * 4 + lines * lineSeparator.length();
+		final StringBuilder buf = new StringBuilder(bufLen);
 		int ip = 0;
 		while (ip < iLen) {
-			int l = Math.min(iLen - ip, blockLen);
+			final int l = Math.min(iLen - ip, blockLen);
 			buf.append(encode(in, iOff + ip, l));
 			buf.append(lineSeparator);
 			ip += l;
@@ -131,26 +142,26 @@ public class Base64Coder {
 	* @param iLen  Number of bytes to process in <code>in</code>, starting at <code>iOff</code>.
 	* @return      A character array containing the Base64 encoded data.
 	*/
-	public static char[] encode(byte[] in, int iOff, int iLen) {
-		int oDataLen = (iLen * 4 + 2) / 3; // output length without padding
-		int oLen = ((iLen + 2) / 3) * 4; // output length including padding
-		char[] out = new char[oLen];
+	public static char[] encode(byte[] in, int iOff, int iLen) { // NOPMD
+		final int oDataLen = (iLen * 4 + 2) / 3; // output length without padding
+		final int oLen = (iLen + 2) / 3 * 4; // output length including padding
+		final char[] out = new char[oLen];
 		int ip = iOff;
-		int iEnd = iOff + iLen;
+		final int iEnd = iOff + iLen;
 		int op = 0;
 		while (ip < iEnd) {
-			int i0 = in[ip++] & 0xff;
-			int i1 = ip < iEnd ? in[ip++] & 0xff : 0;
-			int i2 = ip < iEnd ? in[ip++] & 0xff : 0;
-			int o0 = i0 >>> 2;
-			int o1 = ((i0 & 3) << 4) | (i1 >>> 4);
-			int o2 = ((i1 & 0xf) << 2) | (i2 >>> 6);
-			int o3 = i2 & 0x3F;
-			out[op++] = map1[o0];
-			out[op++] = map1[o1];
-			out[op] = op < oDataLen ? map1[o2] : '=';
+			final int i0 = in[ip++] & 0xff;
+			final int i1 = ip < iEnd ? in[ip++] & 0xff : 0;
+			final int i2 = ip < iEnd ? in[ip++] & 0xff : 0;
+			final int o0 = i0 >>> 2;
+			final int o1 = (i0 & 3) << 4 | i1 >>> 4;
+			final int o2 = (i1 & 0xf) << 2 | i2 >>> 6;
+			final int o3 = i2 & 0x3F;
+			out[op++] = MAP1[o0];
+			out[op++] = MAP1[o1];
+			out[op] = op < oDataLen ? MAP1[o2] : '=';
 			op++;
-			out[op] = op < oDataLen ? map1[o3] : '=';
+			out[op] = op < oDataLen ? MAP1[o3] : '=';
 			op++;
 		}
 		return out;
@@ -161,7 +172,6 @@ public class Base64Coder {
 	* No blanks or line breaks are allowed within the Base64 encoded input data.
 	* @param s  A Base64 String to be decoded.
 	* @return   A String containing the decoded data.
-	* @throws   IllegalArgumentException If the input is not valid Base64 encoded data.
 	*/
 	public static String decodeString(String s) {
 		return new String(decode(s));
@@ -173,15 +183,15 @@ public class Base64Coder {
 	* This method is compatible with <code>sun.misc.BASE64Decoder.decodeBuffer(String)</code>.
 	* @param s  A Base64 String to be decoded.
 	* @return   An array containing the decoded data bytes.
-	* @throws   IllegalArgumentException If the input is not valid Base64 encoded data.
 	*/
 	public static byte[] decodeLines(String s) {
-		char[] buf = new char[s.length()];
+		final char[] buf = new char[s.length()];
 		int p = 0;
 		for (int ip = 0; ip < s.length(); ip++) {
-			char c = s.charAt(ip);
-			if (c != ' ' && c != '\r' && c != '\n' && c != '\t')
+			final char c = s.charAt(ip);
+			if (c != ' ' && c != '\r' && c != '\n' && c != '\t') {
 				buf[p++] = c;
+			}
 		}
 		return decode(buf, 0, p);
 	}
@@ -191,7 +201,6 @@ public class Base64Coder {
 	* No blanks or line breaks are allowed within the Base64 encoded input data.
 	* @param s  A Base64 String to be decoded.
 	* @return   An array containing the decoded data bytes.
-	* @throws   IllegalArgumentException If the input is not valid Base64 encoded data.
 	*/
 	public static byte[] decode(String s) {
 		return decode(s.toCharArray());
@@ -202,7 +211,6 @@ public class Base64Coder {
 	* No blanks or line breaks are allowed within the Base64 encoded input data.
 	* @param in  A character array containing the Base64 encoded data.
 	* @return    An array containing the decoded data bytes.
-	* @throws    IllegalArgumentException If the input is not valid Base64 encoded data.
 	*/
 	public static byte[] decode(char[] in) {
 		return decode(in, 0, in.length);
@@ -215,46 +223,49 @@ public class Base64Coder {
 	* @param iOff  Offset of the first character in <code>in</code> to be processed.
 	* @param iLen  Number of characters to process in <code>in</code>, starting at <code>iOff</code>.
 	* @return      An array containing the decoded data bytes.
-	* @throws      IllegalArgumentException If the input is not valid Base64 encoded data.
 	*/
-	public static byte[] decode(char[] in, int iOff, int iLen) {
-		if (iLen % 4 != 0)
+	// CHECKSTYLE:OFF
+	public static byte[] decode(char[] in, int iOff, int iLen) { // NOPMD
+		// CHECKSTYLE:ON
+		if (iLen % 4 != 0) {
 			throw new IllegalArgumentException(
 					"Length of Base64 encoded input string is not a multiple of 4.");
-		while (iLen > 0 && in[iOff + iLen - 1] == '=')
-			iLen--;
-		int oLen = (iLen * 3) / 4;
-		byte[] out = new byte[oLen];
+		}
+		int iLength = iLen;
+		while (iLength > 0 && in[iOff + iLength - 1] == '=') {
+			iLength--;
+		}
+		final int oLen = iLength * 3 / 4;
+		final byte[] out = new byte[oLen];
 		int ip = iOff;
-		int iEnd = iOff + iLen;
+		final int iEnd = iOff + iLength;
 		int op = 0;
 		while (ip < iEnd) {
-			int i0 = in[ip++];
-			int i1 = in[ip++];
-			int i2 = ip < iEnd ? in[ip++] : 'A';
-			int i3 = ip < iEnd ? in[ip++] : 'A';
-			if (i0 > 127 || i1 > 127 || i2 > 127 || i3 > 127)
+			final int i0 = in[ip++];
+			final int i1 = in[ip++];
+			final int i2 = ip < iEnd ? in[ip++] : 'A';
+			final int i3 = ip < iEnd ? in[ip++] : 'A';
+			if (i0 > 127 || i1 > 127 || i2 > 127 || i3 > 127) {
 				throw new IllegalArgumentException("Illegal character in Base64 encoded data.");
-			int b0 = map2[i0];
-			int b1 = map2[i1];
-			int b2 = map2[i2];
-			int b3 = map2[i3];
-			if (b0 < 0 || b1 < 0 || b2 < 0 || b3 < 0)
+			}
+			final int b0 = MAP2[i0];
+			final int b1 = MAP2[i1];
+			final int b2 = MAP2[i2];
+			final int b3 = MAP2[i3];
+			if (b0 < 0 || b1 < 0 || b2 < 0 || b3 < 0) {
 				throw new IllegalArgumentException("Illegal character in Base64 encoded data.");
-			int o0 = (b0 << 2) | (b1 >>> 4);
-			int o1 = ((b1 & 0xf) << 4) | (b2 >>> 2);
-			int o2 = ((b2 & 3) << 6) | b3;
+			}
+			final int o0 = b0 << 2 | b1 >>> 4;
+			final int o1 = (b1 & 0xf) << 4 | b2 >>> 2;
+			final int o2 = (b2 & 3) << 6 | b3;
 			out[op++] = (byte) o0;
-			if (op < oLen)
+			if (op < oLen) {
 				out[op++] = (byte) o1;
-			if (op < oLen)
+			}
+			if (op < oLen) {
 				out[op++] = (byte) o2;
+			}
 		}
 		return out;
 	}
-
-	// Dummy constructor.
-	private Base64Coder() {
-	}
-
 } // end class Base64Coder
--- javamelody-core/src/test/java/net/bull/javamelody/TestBase64Coder.java
+++ javamelody-core/src/test/java/net/bull/javamelody/TestBase64Coder.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
+import java.io.IOException;
 import java.util.Random;
 
 import org.junit.Test;
@@ -16,10 +17,13 @@ import org.junit.Test;
  * Project home page: <a href="http://www.source-code.biz/base64coder/java/">www.source-code.biz/base64coder/java</a><br>
  * Author: Christian d'Heureuse, Inventec Informatik AG, Zurich, Switzerland<br>
  * Multi-licensed: EPL / LGPL / GPL / AL / BSD / MIT.
+ * Changed on format and java syntax by Emeric Vernat
  */
 public class TestBase64Coder {
 
-	// Test Base64Coder with constant strings.
+	/**
+	 * Test Base64Coder with constant strings.
+	 */
 	@Test
 	public void test1() {
 		check("Aladdin:open sesame", "QWxhZGRpbjpvcGVuIHNlc2FtZQ=="); // example from RFC 2617
@@ -33,52 +37,61 @@ public class TestBase64Coder {
 	}
 
 	private static void check(String plainText, String base64Text) {
-		String s1 = Base64Coder.encodeString(plainText);
-		String s2 = Base64Coder.decodeString(base64Text);
-		if (!s1.equals(base64Text) || !s2.equals(plainText))
+		final String s1 = Base64Coder.encodeString(plainText);
+		final String s2 = Base64Coder.decodeString(base64Text);
+		if (!s1.equals(base64Text) || !s2.equals(plainText)) {
 			fail("Check failed for \"" + plainText + "\" / \"" + base64Text + "\".");
+		}
 	}
 
-	// Test Base64Coder against sun.misc.BASE64Encoder/Decoder with random data.
-	// Line length below 76.
+	/**
+	 * Test Base64Coder against sun.misc.BASE64Encoder/Decoder with random data.
+	 * Line length below 76.
+	 * @throws IOException e
+	 */
+	@SuppressWarnings("restriction")
 	@Test
-	public void test2() throws Exception {
+	public void test2() throws IOException {
 		final int maxLineLen = 76 - 1; // the Sun encoder adds a CR/LF when a line is longer
-		final int maxDataBlockLen = (maxLineLen * 3) / 4;
-		sun.misc.BASE64Encoder sunEncoder = new sun.misc.BASE64Encoder();
-		sun.misc.BASE64Decoder sunDecoder = new sun.misc.BASE64Decoder();
-		Random rnd = new Random(0x538afb92);
+		final int maxDataBlockLen = maxLineLen * 3 / 4;
+		final sun.misc.BASE64Encoder sunEncoder = new sun.misc.BASE64Encoder();
+		final sun.misc.BASE64Decoder sunDecoder = new sun.misc.BASE64Decoder();
+		final Random rnd = new Random(0x538afb92);
 		for (int i = 0; i < 50000; i++) {
-			int len = rnd.nextInt(maxDataBlockLen + 1);
-			byte[] b0 = new byte[len];
+			final int len = rnd.nextInt(maxDataBlockLen + 1);
+			final byte[] b0 = new byte[len];
 			rnd.nextBytes(b0);
-			String e1 = new String(Base64Coder.encode(b0));
-			String e2 = sunEncoder.encode(b0);
-			assertEquals(e2, e1);
-			byte[] b1 = Base64Coder.decode(e1);
-			byte[] b2 = sunDecoder.decodeBuffer(e2);
+			final String e1 = new String(Base64Coder.encode(b0));
+			final String e2 = sunEncoder.encode(b0);
+			assertEquals("test2", e2, e1);
+			final byte[] b1 = Base64Coder.decode(e1);
+			final byte[] b2 = sunDecoder.decodeBuffer(e2);
 			assertArrayEquals(b0, b1);
 			assertArrayEquals(b0, b2);
 		}
 	}
 
-	// Test Base64Coder line encoding/decoding against sun.misc.BASE64Encoder/Decoder
-	// with random data.
+	/**
+	 * Test Base64Coder line encoding/decoding against sun.misc.BASE64Encoder/Decoder
+	 * with random data.
+	 * @throws IOException e
+	 */
+	@SuppressWarnings("restriction")
 	@Test
-	public void test3() throws Exception {
+	public void test3() throws IOException {
 		final int maxDataBlockLen = 512;
-		sun.misc.BASE64Encoder sunEncoder = new sun.misc.BASE64Encoder();
-		sun.misc.BASE64Decoder sunDecoder = new sun.misc.BASE64Decoder();
-		Random rnd = new Random(0x39ac7d6e);
+		final sun.misc.BASE64Encoder sunEncoder = new sun.misc.BASE64Encoder();
+		final sun.misc.BASE64Decoder sunDecoder = new sun.misc.BASE64Decoder();
+		final Random rnd = new Random(0x39ac7d6e);
 		for (int i = 0; i < 10000; i++) {
-			int len = rnd.nextInt(maxDataBlockLen + 1);
-			byte[] b0 = new byte[len];
+			final int len = rnd.nextInt(maxDataBlockLen + 1);
+			final byte[] b0 = new byte[len];
 			rnd.nextBytes(b0);
-			String e1 = new String(Base64Coder.encodeLines(b0));
-			String e2 = sunEncoder.encodeBuffer(b0);
-			assertEquals(e2, e1);
-			byte[] b1 = Base64Coder.decodeLines(e1);
-			byte[] b2 = sunDecoder.decodeBuffer(e2);
+			final String e1 = Base64Coder.encodeLines(b0);
+			final String e2 = sunEncoder.encodeBuffer(b0);
+			assertEquals("test3", e2, e1);
+			final byte[] b1 = Base64Coder.decodeLines(e1);
+			final byte[] b2 = sunDecoder.decodeBuffer(e2);
 			assertArrayEquals(b0, b1);
 			assertArrayEquals(b0, b2);
 		}
