ee83171b506ca86a74b6ed56db2e7cf68f6cf31b
evernat <evernat@free.fr>
2012-10-20 20:14:13 +0000

    fix for issue 261: use thread context-classloader to load jdbc-driver

--- javamelody-core/src/main/java/net/bull/javamelody/JdbcDriver.java
+++ javamelody-core/src/main/java/net/bull/javamelody/JdbcDriver.java
@@ -70,7 +70,10 @@ public class JdbcDriver implements Driver {
 			return null;
 		}
 		try {
-			Class.forName(proxiedDriver);
+			// on utilise Thread.currentThread().getContextClassLoader() car le driver peut ne pas être
+			// dans le même classLoader que les classes de javamelody
+			Class.forName(proxiedDriver, true, Thread.currentThread().getContextClassLoader());
+			// et non Class.forName(proxiedDriver);
 		} catch (final ClassNotFoundException e) {
 			// Rq: le constructeur de SQLException avec message et cause n'existe qu'en jdk 1.6
 			final SQLException ex = new SQLException(e.getMessage());
