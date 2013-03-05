a6bd59b48b25f9a9fde76a5d2e05d6cea0c5cc60
evernat <evernat@free.fr>
2013-03-05 16:23:23 +0000

    fix : when opening the detail of a Quartz job, display the statistics of the job including sql or services called + when looking for the usages of a sql or service, include results for jobs

--- javamelody-core/src/main/java/net/bull/javamelody/HtmlCounterReport.java
+++ javamelody-core/src/main/java/net/bull/javamelody/HtmlCounterReport.java
@@ -88,7 +88,7 @@ class HtmlCounterReport extends HtmlAbstractReport {
 
 		void writeRequestAndGraphDetail(Collector collector, CollectorServer collectorServer,
 				String graphName) throws IOException {
-			counters = collector.getRangeCountersToBeDisplayed(range);
+			counters = collector.getRangeCounters(range);
 			requestsById = mapAllRequestsById();
 			final CounterRequest request = requestsById.get(graphName);
 			if (request != null) {
@@ -159,7 +159,7 @@ class HtmlCounterReport extends HtmlAbstractReport {
 
 		void writeRequestUsages(Collector collector, String requestId) throws IOException {
 			assert requestId != null;
-			counters = collector.getRangeCountersToBeDisplayed(range);
+			counters = collector.getRangeCounters(range);
 			CounterRequest myRequest = null;
 			final List<CounterRequest> requests = new ArrayList<CounterRequest>();
 			for (final Counter counter : counters) {
