3176dd7cd18993d2d6f971680160b08da6b529aa
evernat@free.fr <evernat@free.fr@80d463d6-a79d-11de-ba6e-69e41c8db662>
2013-04-09 22:13:50 +0000

    fix: adding an application in the collect server does not work, after restart of the collect server, if the access to the application data is secured by basic authentication with the documented login "monitoring"

--- trunk/javamelody-core/src/main/java/net/bull/javamelody/Parameters.java
+++ trunk/javamelody-core/src/main/java/net/bull/javamelody/Parameters.java
@@ -142,7 +142,7 @@ final class Parameters {
 			for (final URL url : urls) {
 				final String urlString = url.toString();
 				// on enlève le suffixe ajouté précédemment dans parseUrl
-				sb.append(urlString.substring(0, urlString.indexOf("/monitoring"))).append(',');
+				sb.append(urlString.substring(0, urlString.lastIndexOf("/monitoring"))).append(',');
 			}
 			sb.delete(sb.length() - 1, sb.length());
 			properties.put(entry.getKey(), sb.toString());
