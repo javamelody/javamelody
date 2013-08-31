9c122c9bcb2e4c31667c41405fceca7b9b32b067
evernat@free.fr <evernat@free.fr@80d463d6-a79d-11de-ba6e-69e41c8db662>
2013-08-31 19:58:35 +0000

    fix issue 335: centralized server not compatible with ehcache 2.7.2

--- trunk/javamelody-core/src/main/java/net/bull/javamelody/CacheInformations.java
+++ trunk/javamelody-core/src/main/java/net/bull/javamelody/CacheInformations.java
@@ -116,7 +116,7 @@ class CacheInformations implements Serializable {
 		}
 	}
 
-	private static long invokeStatisticsMethod(Statistics statistics, String methodName) {
+	private static long invokeStatisticsMethod(Object statistics, String methodName) {
 		try {
 			// getInMemoryHits, getCacheHits et getCacheMisses existent en v1.2.1 et v1.2.3
 			// mais avec int comme résultat et existent depuis v1.2.4 avec long comme résultat
