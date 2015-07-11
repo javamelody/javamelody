b08ae9e456ef73fb80cdfe02a64c04bb1854e530
evernat <evernat@free.fr>
2015-07-11 23:11:07 +0200

    initial spring-boot app

---
+++ javamelody-for-spring-boot/src/main/java/hello/Application.java
@@ -0,0 +1,24 @@
+package hello;
+
+import java.util.Arrays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = SpringApplication.run(Application.class, args);
+
+		System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+		String[] beanNames = ctx.getBeanDefinitionNames();
+		Arrays.sort(beanNames);
+		for (String beanName : beanNames) {
+			System.out.println(beanName);
+		}
+	}
+
+}
---
+++ javamelody-for-spring-boot/src/main/java/hello/HelloController.java
@@ -0,0 +1,14 @@
+package hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@RequestMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot!";
+	}
+
+}
