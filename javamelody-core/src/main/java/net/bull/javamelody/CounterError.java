751ac6aee66206ea8c7550224ed6177a89aa0fa1
zvrablik <vrablik@gmail.com>
2016-04-22 15:18:04 +0200

    use parameters to set max message and stack trace size

--- javamelody-core/src/main/java/net/bull/javamelody/CounterError.java
+++ javamelody-core/src/main/java/net/bull/javamelody/CounterError.java
@@ -32,6 +32,13 @@ class CounterError implements Serializable {
 	private static final long serialVersionUID = 5690702786722045646L;
 	@SuppressWarnings("all")
 	private static final ThreadLocal<HttpServletRequest> HTTP_SERVLET_REQUEST_CONTEXT = new ThreadLocal<HttpServletRequest>();
+
+	/**
+	 * Default max size of error message.
+	 */
+	public static final int DEFAULT_MESSAGE_MAX_SIZE = 1000;
+	public static final int DEFAULT_STACKTRACE_MAX_SIZE = 10000;
+
 	private final long time;
 	private final String remoteUser;
 	private final String httpRequest;
@@ -42,12 +49,42 @@ class CounterError implements Serializable {
 		super();
 		assert message != null;
 		this.time = System.currentTimeMillis();
-		if (message != null && message.length() > 1024){
-			message = message.substring(0, 1024);//avoid possible memory errors as javamelody store 100 errors in memory
+
+		String messageMaxSizeText = Parameters.getParameterByName("log.size.message");
+		int messageMaxSize;
+		if (messageMaxSizeText == null || messageMaxSizeText.isEmpty()){
+			messageMaxSize = DEFAULT_MESSAGE_MAX_SIZE;
+		} else {
+			try {
+				messageMaxSize = Integer.parseInt(messageMaxSizeText);
+			} catch (NumberFormatException e){
+				messageMaxSize = DEFAULT_MESSAGE_MAX_SIZE;
+				LOG.warn("Error message max size is not correct number. Used default value. Invalid value:" + messageMaxSizeText, e);
+			}
+		}
+		LOG.info("Max error message size " + messageMaxSize);
+
+		if ( message.length() > messageMaxSize){
+			message = message.substring(0, messageMaxSize);//avoid possible memory errors as javamelody store 100 errors in memory
 		}
 		this.message = message;
-		if (stackTrace != null && stackTrace.length() > 10000){
-			stackTrace = stackTrace.substring(0, 10000);
+
+		String stackTraceMaxSizeText = Parameters.getParameterByName("log.size.stacktrace");
+		int stackTraceMaxSize;
+		if (stackTraceMaxSizeText == null || stackTraceMaxSizeText.isEmpty()){
+			stackTraceMaxSize = DEFAULT_STACKTRACE_MAX_SIZE;
+		} else {
+			try {
+				stackTraceMaxSize = Integer.parseInt(stackTraceMaxSizeText);
+			} catch (NumberFormatException e){
+				stackTraceMaxSize = DEFAULT_STACKTRACE_MAX_SIZE;
+				LOG.warn("Error stack trace max size is not correct number. Used default value. Invalid value:" + stackTraceMaxSizeText, e);
+			}
+		}
+		LOG.info("Max stack trace size " + stackTraceMaxSize);
+
+		if (stackTrace != null && stackTrace.length() > stackTraceMaxSize){
+			stackTrace = stackTrace.substring(0, stackTraceMaxSize);
 		}
 		this.stackTrace = stackTrace;
 		final HttpServletRequest currentRequest = getCurrentRequest();
