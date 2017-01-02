4a09a41038c327844da070aa6adaf573dbd94409
evernat <evernat@free.fr>
2017-01-02 12:19:12 +0100

    fix multiple prefix in batch requestName

--- javamelody-core/src/main/java/net/bull/javamelody/JdbcWrapper.java
+++ javamelody-core/src/main/java/net/bull/javamelody/JdbcWrapper.java
@@ -121,7 +121,8 @@ public final class JdbcWrapper {
 					// executeUpdate(String, ...) ou execute(String sql),
 					// alors la requête sql est le premier argument (et pas query)
 					requestName = (String) args[0];
-				} else if ("executeBatch".equals(methodName)) {
+				} else if ("executeBatch".equals(methodName)
+						&& !requestName.startsWith("/* BATCH */ ")) {
 					// if executeBatch, add a prefix in the request name to explain that 
 					// 1 batch "hit" is equivalent to several exec of the request in the db
 					requestName = "/* BATCH */ " + requestName;
