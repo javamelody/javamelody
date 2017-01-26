daa32366f5fd11b6bf80158bcc82c60d26cbc625
evernat <evernat@free.fr>
2017-01-26 14:19:05 +0100

    fix unit tests using Ant

--- javamelody-core/src/test/java/net/bull/javamelody/TestMongoWrapper.java
+++ javamelody-core/src/test/java/net/bull/javamelody/TestMongoWrapper.java
@@ -24,11 +24,12 @@ import static org.easymock.EasyMock.verify;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
+import org.apache.log4j.Logger;
 import org.bson.Document;
+import org.bson.codecs.configuration.CodecRegistry;
 import org.junit.Before;
 import org.junit.Test;
 
-import com.mongodb.ReadConcern;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 
@@ -47,26 +48,37 @@ public class TestMongoWrapper {
 	@SuppressWarnings("unchecked")
 	@Test
 	public void testCreateDatabaseProxy() {
+		try {
+			Class.forName("com.mongodb.ReadPreference");
+		} catch (final ClassNotFoundException e) {
+			Logger.getRootLogger().info(e.toString());
+			// si mongodb-driver-core n'est pas disponible dans le classpath (test depuis Ant),
+			// on ne peut pas exécuter ce test
+			return;
+		}
 		final MongoDatabase database = createNiceMock(MongoDatabase.class);
 		final MongoCollection<Document> collection = createNiceMock(MongoCollection.class);
-		expect(database.withReadConcern(ReadConcern.MAJORITY)).andReturn(database).anyTimes();
+		final CodecRegistry codecRegistry = createNiceMock(CodecRegistry.class);
+		expect(database.withCodecRegistry(codecRegistry)).andReturn(database).anyTimes();
 		expect(database.getCollection("collection")).andReturn(collection).anyTimes();
 		expect(database.getName()).andReturn("db").anyTimes();
-		expect(collection.withReadConcern(ReadConcern.MAJORITY)).andReturn(collection).anyTimes();
+		expect(collection.withCodecRegistry(codecRegistry)).andReturn(collection).anyTimes();
 
 		replay(database);
 		replay(collection);
+		replay(codecRegistry);
 		final MongoDatabase databaseProxy = MongoWrapper.createDatabaseProxy(database);
 		assertNotNull("createDatabaseProxy", databaseProxy);
-		assertNotNull("databaseProxy", databaseProxy.withReadConcern(ReadConcern.MAJORITY));
+		assertNotNull("databaseProxy", databaseProxy.withCodecRegistry(codecRegistry));
 		assertNotNull("getCollection", databaseProxy.getCollection("collection"));
 		assertEquals("getName", "db", databaseProxy.getName());
 
 		assertNotNull("collectionProxy",
-				databaseProxy.getCollection("collection").withReadConcern(ReadConcern.MAJORITY));
-		databaseProxy.getCollection("collection").getReadConcern();
+				databaseProxy.getCollection("collection").withCodecRegistry(codecRegistry));
+		databaseProxy.getCollection("collection").getCodecRegistry();
 		databaseProxy.getCollection("collection").find();
 		verify(database);
 		verify(collection);
+		verify(codecRegistry);
 	}
 }
