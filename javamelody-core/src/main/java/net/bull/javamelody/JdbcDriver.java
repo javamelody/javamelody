f296414bfda82d4b95b4b90ba973e6c3e6d7d02d
evernat <evernat@free.fr>
2017-05-04 01:44:07 +0200

    fix usedConnectionCount (jdbc connections inside datasources must not be proxied)

--- javamelody-core/src/main/java/net/bull/javamelody/JdbcDriver.java
+++ javamelody-core/src/main/java/net/bull/javamelody/JdbcDriver.java
@@ -67,14 +67,10 @@ public class JdbcDriver implements Driver {
 			// (we pass here from the DriverManager.getConnection below)
 			return null;
 		}
-		final Properties myInfo = (Properties) info.clone();
-		myInfo.put("javamelody", "false");
 		String myUrl = url;
 		// we load first the driver class from the info or the url, to be sure that it will be found
-		String proxiedDriver = myInfo.getProperty("driver");
-		if (proxiedDriver != null) {
-			myInfo.remove("driver");
-		} else if (myUrl != null) {
+		String proxiedDriver = info.getProperty("driver");
+		if (proxiedDriver == null && myUrl != null) {
 			// if not in the info, the driver class could also be passed at the end of the url, for example ...?driver=org.h2.Driver
 			final int index = myUrl.indexOf("driver=");
 			if (index != -1) {
@@ -82,21 +78,27 @@ public class JdbcDriver implements Driver {
 				myUrl = myUrl.substring(0, index - 1);
 			}
 		}
-		if (proxiedDriver != null) {
-			try {
-				// on utilise Thread.currentThread().getContextClassLoader() car le driver peut ne pas être
-				// dans le même classLoader que les classes de javamelody
-				// Class driverClass =
-				Class.forName(proxiedDriver, true, Thread.currentThread().getContextClassLoader());
-				// et non Class.forName(proxiedDriver);
-			} catch (final ClassNotFoundException e) {
-				throw new SQLException(e.getMessage(), e);
-			}
+		if (proxiedDriver == null) {
+			// if the driver is not defined in the info or in the url
+			// it could still be found automatically if the driver is in the classpath
+			// or (in WEB-INF/lib and if the jdbc drivers are not loaded by the JDK before this webapp)
+			// but we don't want to create proxies and increment counts for the connections inside datasources
+			// so we only accept and go further if driver is defined in the info or in the url
+			return null;
 		}
-		// but if the driver is not defined in the info or in the url
-		// it will still be found automatically if the driver is in the classpath
-		// or (in WEB-INF/lib and if the jdbc drivers are not loaded by the JDK before this webapp)
+		try {
+			// on utilise Thread.currentThread().getContextClassLoader() car le driver peut ne pas être
+			// dans le même classLoader que les classes de javamelody
+			// Class driverClass =
+			Class.forName(proxiedDriver, true, Thread.currentThread().getContextClassLoader());
+			// et non Class.forName(proxiedDriver);
+		} catch (final ClassNotFoundException e) {
+			throw new SQLException(e.getMessage(), e);
+		}
 
+		final Properties myInfo = (Properties) info.clone();
+		myInfo.remove("driver");
+		myInfo.put("javamelody", "false");
 		Parameters.initJdbcDriverParameters(myUrl, myInfo);
 		// we could call driverClass.newInstance().connect(myUrl, myInfo)
 		// possibly by looking the driver which accepts the url in DriverManager.getDrivers()
