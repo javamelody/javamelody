c7ee27258d72559092959f9de22c8e4f2adfba07
evernat <evernat@free.fr>
2017-06-18 22:54:52 +0200

    follow-up to internal classes move

--- javamelody-core/src/main/java/net/bull/javamelody/internal/model/JavaInformations.java
+++ javamelody-core/src/main/java/net/bull/javamelody/internal/model/JavaInformations.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package net.bull.javamelody.internal.model;
+package net.bull.javamelody.internal.model; // NOPMD
 
 import java.io.Serializable;
 import java.lang.management.ManagementFactory;
