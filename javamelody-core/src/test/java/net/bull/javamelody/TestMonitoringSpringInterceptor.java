5729835a2106f6d492f92de9132d9838d36996a9
evernat <evernat@free.fr>
2017-08-01 00:16:44 +0200

    fix unit test

--- javamelody-core/src/test/java/net/bull/javamelody/TestMonitoringSpringInterceptor.java
+++ javamelody-core/src/test/java/net/bull/javamelody/TestMonitoringSpringInterceptor.java
@@ -330,7 +330,8 @@ public class TestMonitoringSpringInterceptor {
 		assertNotNull("methodMatcher", pointcut.getMethodMatcher());
 		assertFalse("methodMatcher.isRuntime", pointcut.getMethodMatcher().isRuntime());
 		try {
-			assertFalse("methodMatcher.matches", pointcut.getMethodMatcher().matches(null, null));
+			assertFalse("methodMatcher.matches",
+					pointcut.getMethodMatcher().matches(null, null, (Object[]) null));
 		} catch (final UnsupportedOperationException e) {
 			assertNotNull("ok", e);
 		}
