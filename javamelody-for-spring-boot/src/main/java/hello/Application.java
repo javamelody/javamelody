b7eed7d0a8472ccfddd9eb0e1f1a5989d2f5afee
evernat <evernat@free.fr>
2018-04-18 21:18:11 +0200

    Upgrade to Spring-boot 2

--- javamelody-for-spring-boot/src/main/java/hello/Application.java
+++ javamelody-for-spring-boot/src/main/java/hello/Application.java
@@ -2,11 +2,10 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SuppressWarnings("javadoc")
 @SpringBootApplication
-public class Application extends SpringBootServletInitializer {
+public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
