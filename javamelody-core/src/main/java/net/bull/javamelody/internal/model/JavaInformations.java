7146d6dd5142aa2632473c2d3d369a84b8332a73
evernat <evernat@free.fr>
2018-09-26 23:48:19 +0200

    fix #779 JSVC and errno: 13 error: Unable to open directory /proc/self/fd

--- javamelody-core/src/main/java/net/bull/javamelody/internal/model/JavaInformations.java
+++ javamelody-core/src/main/java/net/bull/javamelody/internal/model/JavaInformations.java
@@ -248,8 +248,9 @@ public class JavaInformations implements Serializable { // NOPMD
 	private static long buildOpenFileDescriptorCount() {
 		try {
 			return MBeansAccessor.getLongFromOperatingSystem("OpenFileDescriptorCount");
-		} catch (final Error e) {
-			// pour issue 16 (using jsvc on ubuntu or debian)
+		} catch (final Throwable e) { // NOPMD
+			// pour issues 16, 779 (using jsvc on ubuntu or debian)
+			// a priori, InternalError est catchée par le MBeanServer et encapsulé en RuntimeErrorException
 			return -1;
 		}
 	}
@@ -257,8 +258,9 @@ public class JavaInformations implements Serializable { // NOPMD
 	private static long buildMaxFileDescriptorCount() {
 		try {
 			return MBeansAccessor.getLongFromOperatingSystem("MaxFileDescriptorCount");
-		} catch (final Error e) {
-			// pour issue 16 (using jsvc on ubuntu or debian)
+		} catch (final Throwable e) { // NOPMD
+			// pour issues 16, 779 (using jsvc on ubuntu or debian)
+			// a priori, InternalError est catchée par le MBeanServer et encapsulé en RuntimeErrorException
 			return -1;
 		}
 	}
