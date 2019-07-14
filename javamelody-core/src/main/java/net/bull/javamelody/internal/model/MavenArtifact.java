817df4def5b93b2a2490a2bab6f3c45ea675d305
evernat <evernat@free.fr>
2019-07-14 21:35:17 +0200

    refactoring

--- javamelody-core/src/main/java/net/bull/javamelody/internal/model/MavenArtifact.java
+++ javamelody-core/src/main/java/net/bull/javamelody/internal/model/MavenArtifact.java
@@ -349,11 +349,15 @@ public final class MavenArtifact implements Serializable {
 
 	private boolean isContained(List<MavenArtifact> artifacts) {
 		for (final MavenArtifact artifact : artifacts) {
-			if (groupId.equals(artifact.groupId) && artifactId.equals(artifact.artifactId)) {
+			if (isSame(artifact)) {
 				return true;
 			}
 		}
 		return false;
+	}
+
+	private boolean isSame(MavenArtifact artifact) {
+		return groupId.equals(artifact.groupId) && artifactId.equals(artifact.artifactId);
 	}
 
 	private String getPath(String extension) {
