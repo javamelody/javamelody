1e39fe209f90882339e8916b781959e5805ef923
Eugene Kortov <kortovea@gmail.com>
2019-11-19 02:48:32 +0400

    Add sessionId to CounterRequestContext (#873)
    - look further to get the remote user into the current requests
    - add logic to get remoteUser and sessionId from httpRequest
    - add sessionId to CounterRequestContext (for External api)
    - remove remoteUser from bindContext method (since it is already in a httpRequest)
    - fix classes and tests which use bindContext method

--- javamelody-core/src/main/java/net/bull/javamelody/internal/model/CounterRequestContext.java
+++ javamelody-core/src/main/java/net/bull/javamelody/internal/model/CounterRequestContext.java
@@ -52,6 +52,7 @@ public class CounterRequestContext implements ICounterRequestContext, Cloneable,
 	private final long startTime;
 	private final long startCpuTime;
 	private final long startAllocatedBytes;
+	private final String sessionId;
 	// ces 2 champs sont initialisés à 0
 	private int childHits;
 	private int childDurationsSum;
@@ -61,11 +62,11 @@ public class CounterRequestContext implements ICounterRequestContext, Cloneable,
 	// CHECKSTYLE:OFF
 	public CounterRequestContext(Counter parentCounter, CounterRequestContext parentContext,
 			String requestName, String completeRequestName, HttpServletRequest httpRequest,
-			String remoteUser, long startCpuTime, long startAllocatedBytes) {
+			String remoteUser, long startCpuTime, long startAllocatedBytes, String sessionId) {
 		// CHECKSTYLE:ON
 		this(parentCounter, parentContext, requestName, completeRequestName, httpRequest,
 				remoteUser, Thread.currentThread().getId(), System.currentTimeMillis(),
-				startCpuTime, startAllocatedBytes);
+				startCpuTime, startAllocatedBytes, sessionId);
 		if (parentContext != null) {
 			parentContext.setCurrentChildContext(this);
 		}
@@ -76,7 +77,7 @@ public class CounterRequestContext implements ICounterRequestContext, Cloneable,
 	private CounterRequestContext(Counter parentCounter, CounterRequestContext parentContext, // NOPMD
 			String requestName, String completeRequestName, HttpServletRequest httpRequest,
 			String remoteUser, long threadId, long startTime, long startCpuTime,
-			long startAllocatedBytes) {
+			long startAllocatedBytes, String sessionId) {
 		// CHECKSTYLE:ON
 		super();
 		assert parentCounter != null;
@@ -94,6 +95,7 @@ public class CounterRequestContext implements ICounterRequestContext, Cloneable,
 		this.startTime = startTime;
 		this.startCpuTime = startCpuTime;
 		this.startAllocatedBytes = startAllocatedBytes;
+		this.sessionId = sessionId;
 	}
 
 	public Counter getParentCounter() {
@@ -330,7 +332,7 @@ public class CounterRequestContext implements ICounterRequestContext, Cloneable,
 		//				counter.getIconName(), counter.getChildCounterName(), null);
 		final CounterRequestContext clone = new CounterRequestContext(counter, parentContextClone,
 				getRequestName(), getCompleteRequestName(), httpRequest, getRemoteUser(),
-				getThreadId(), startTime, startCpuTime, startAllocatedBytes);
+				getThreadId(), startTime, startCpuTime, startAllocatedBytes, sessionId);
 		clone.childHits = getChildHits();
 		clone.childDurationsSum = getChildDurationsSum();
 		final CounterRequestContext childContext = getCurrentChildContext();
