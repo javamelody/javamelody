3c59405fdd7527bee030a35027a05ede3cf37400
evernat <evernat@free.fr>
2020-01-13 02:19:49 +0100

    unit tests

--- javamelody-core/src/test/java/net/bull/javamelody/TestSessionListener.java
+++ javamelody-core/src/test/java/net/bull/javamelody/TestSessionListener.java
@@ -59,7 +59,11 @@ public class TestSessionListener {
 	}
 
 	private void clearSessions() {
-		sessionListener.contextDestroyed(null);
+		final ServletContext servletContext = createNiceMock(ServletContext.class);
+		final ServletContextEvent servletContextEvent = new ServletContextEvent(servletContext);
+		replay(servletContext);
+		sessionListener.contextDestroyed(servletContextEvent);
+		verify(servletContext);
 	}
 
 	/** Test. */
@@ -174,8 +178,12 @@ public class TestSessionListener {
 	/** Test. */
 	@Test
 	public void testContextDestroyed() {
+		final ServletContext servletContext = createNiceMock(ServletContext.class);
+		final ServletContextEvent servletContextEvent = new ServletContextEvent(servletContext);
+		replay(servletContext);
 		sessionListener.sessionCreated(createSessionEvent());
-		sessionListener.contextDestroyed(null);
+		sessionListener.contextDestroyed(servletContextEvent);
+		verify(servletContext);
 		if (!SessionListener.getAllSessionsInformations().isEmpty()) {
 			fail("contextDestroyed");
 		}
