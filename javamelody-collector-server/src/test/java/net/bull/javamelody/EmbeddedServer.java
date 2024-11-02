e7e5a5be8d9889cddb3e0410c6dc03d3975ea6a4
emeric.vernat <emeric.vernat@kleegroup.com>
2024-11-02 16:40:02 +0100

    upgrade embedded server to winstone 8.2 (jetty 12) in optional collector server

--- javamelody-collector-server/src/test/java/net/bull/javamelody/EmbeddedServer.java
+++ javamelody-collector-server/src/test/java/net/bull/javamelody/EmbeddedServer.java
@@ -1,12 +1,13 @@
 package net.bull.javamelody;
 
+import org.eclipse.jetty.ee10.servlet.ServletHolder;
 import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.handler.ContextHandlerCollection;
-import org.eclipse.jetty.server.handler.HandlerCollection;
-import org.eclipse.jetty.server.handler.RequestLogHandler;
-import org.eclipse.jetty.servlet.ServletContextHandler;
-import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.server.handler.InetAccessHandler;
+import org.eclipse.jetty.ee10.webapp.WebAppContext;
+import org.eclipse.jetty.util.resource.ResourceFactory;
+
+import java.net.URI;
 
 /**
  * Embedded http server including javamelody reports.
@@ -24,21 +25,23 @@ public class EmbeddedServer {
 	public static void start(int port) throws Exception {
 		// Init jetty
 		final Server server = new Server(port);
-		final ContextHandlerCollection contexts = new ContextHandlerCollection();
-		final ServletContextHandler context = new ServletContextHandler(contexts, "/");
+		final InetAccessHandler inetAccessHandler = new InetAccessHandler();
+		server.setHandler(inetAccessHandler);
+
+		final Handler.Sequence handlers = new Handler.Sequence();
+		inetAccessHandler.setHandler(handlers);
+
+		final WebAppContext webapp = new WebAppContext();
+		webapp.setContextPath("/");
+		webapp.setWar("javamelody-collector-server/src/main/webapp");
 
 		// servlet of the javamelody collector server
 		final ServletHolder servletHolder = new ServletHolder(new CollectorServlet());
+		webapp.addServlet(servletHolder, "/*");
 
-		context.addServlet(servletHolder, "/*");
-
-		final RequestLogHandler requestLogHandler = new RequestLogHandler();
-		contexts.addHandler(requestLogHandler);
-
-		final HandlerCollection handlers = new HandlerCollection();
-		handlers.setHandlers(new Handler[] { contexts });
-		server.setHandler(handlers);
+		handlers.addHandler(webapp);
 
 		server.start();
+		server.join();
 	}
 }
