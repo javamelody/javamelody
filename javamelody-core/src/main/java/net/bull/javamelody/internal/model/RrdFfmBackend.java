8d4de8375d074cb9e2a76b02d86fa0be0e56559c
evernat <evernat@free.fr>
2025-11-14 00:16:32 +0100

    import

--- javamelody-core/src/main/java/net/bull/javamelody/internal/model/RrdFfmBackend.java
+++ javamelody-core/src/main/java/net/bull/javamelody/internal/model/RrdFfmBackend.java
@@ -23,6 +23,7 @@ import org.jrobin.core.RrdFileBackend;
 import java.io.IOException;
 import java.lang.foreign.Arena;
 import java.lang.foreign.MemorySegment;
+import java.lang.foreign.ValueLayout;
 import java.nio.channels.FileChannel;
 import java.util.Timer;
 import java.util.TimerTask;
